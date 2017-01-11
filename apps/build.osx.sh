57a6b55209768d08ece8f93bbc8e998794afd966
Fabio Pellacini <fabio.pellacini@gmail.com>
2017-01-11 18:08:08 +0100

    fix more build issues on multiple platforms

--- apps/build.osx.sh
+++ apps/build.osx.sh
@@ -1,15 +1,10 @@
-CXX=clang++
-CXXFLAGS='-std=c++14 -stdlib=libc++ -O3 -g'
-OPENGL_LFLAGS='-I/usr/local/include -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -L/usr/local/lib -lglfw3'
-
 mkdir -p bin
 
-$CXX $CXXFLAGS -o bin/ysym apps/ysym.cpp
-$CXX $CXXFLAGS -o bin/ytestgen apps/ytestgen.cpp
-$CXX $CXXFLAGS -o bin/ytrace apps/ytrace.cpp
-$CXX $CXXFLAGS -o bin/yobj2gltf apps/yobj2gltf.cpp
-
-$CXX $CXXFLAGS $OPENGL_LFLAGS -o bin/yshade apps/yshade.cpp
-$CXX $CXXFLAGS $OPENGL_LFLAGS -o bin/yisym apps/yisym.cpp
-$CXX $CXXFLAGS $OPENGL_LFLAGS -o bin/yitrace apps/yitrace.cpp
-$CXX $CXXFLAGS $OPENGL_LFLAGS -o bin/yimview apps/yimview.cpp
+clang++ -MMD -MF bin/ysym.o.d -std=c++14 -stdlib=libc++ -O3 -g -fcolor-diagnostics -I/usr/local/include -c -o bin/ysym.o apps/ysym.cpp
+clang++ -o bin/ysym bin/ysym.o -std=c++14
+clang++ -MMD -MF bin/ytestgen.o.d -std=c++14 -stdlib=libc++ -O3 -g -fcolor-diagnostics -I/usr/local/include -c -o bin/ytestgen.o apps/ytestgen.cpp
+clang++ -o bin/ytestgen bin/ytestgen.o -std=c++14
+clang++ -MMD -MF bin/ytrace.o.d -std=c++14 -stdlib=libc++ -O3 -g -fcolor-diagnostics -I/usr/local/include -c -o bin/ytrace.o apps/ytrace.cpp
+clang++ -o bin/ytrace bin/ytrace.o -std=c++14
+clang++ -MMD -MF bin/yobj2gltf.o.d -std=c++14 -stdlib=libc++ -O3 -g -fcolor-diagnostics -I/usr/local/include -c -o bin/yobj2gltf.o apps/yobj2gltf.cpp
+clang++ -o bin/yobj2gltf bin/yobj2gltf.o -std=c++14
