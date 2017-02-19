41e0d741a5be57d8e060b90b1022cb8f6874b13c
Fabio Pellacini <fabio.pellacini@gmail.com>
2017-02-19 14:11:38 +0100

    intropducing yocto_img for simpler image load/unload

---
+++ yocto/yocto_img.cpp
@@ -0,0 +1,222 @@
+//
+// LICENSE:
+//
+// Copyright (c) 2016 -- 2017 Fabio Pellacini
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+// -----------------------------------------------------------------------------
+// IMPLEMENTATION OF YOCTO_IMG
+// -----------------------------------------------------------------------------
+
+#include "yocto_img.h"
+
+#include <cmath>
+
+#ifndef YIMG_NO_STBIMAGE
+
+#ifndef _WIN32
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#ifndef __clang__
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_STATIC
+#include "ext/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
+#include "ext/stb_image_write.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_STATIC
+#include "ext/stb_image_resize.h"
+
+#ifndef _WIN32
+#pragma GCC diagnostic pop
+#endif
+
+#endif
+
+namespace yimg {
+
+//
+// Get extension (including '.').
+//
+static inline std::string _get_extension(const std::string& filename) {
+    auto pos = filename.rfind('.');
+    if (pos == std::string::npos) return "";
+    return filename.substr(pos);
+}
+
+//
+// Loads an image
+//
+YIMG_API simage* load_image(const std::string& filename) {
+    auto img = new simage();
+    if (_get_extension(filename) == ".hdr") {
+        img->hdr = stbi_loadf(filename.c_str(), &img->width, &img->height,
+                              &img->ncomp, 0);
+    } else {
+        img->ldr = stbi_load(filename.c_str(), &img->width, &img->height,
+                             &img->ncomp, 0);
+    }
+    if (!img->ldr && !img->hdr) {
+        delete img;
+        throw std::runtime_error("cannot load image " + filename);
+    }
+    return img;
+}
+
+//
+// Loads an image
+//
+YIMG_API void load_image(const std::string& filename, int& width, int& height,
+                         int& ncomp, float*& hdr, byte*& ldr) {
+    if (_get_extension(filename) == ".hdr") {
+        hdr = stbi_loadf(filename.c_str(), &width, &height, &ncomp, 0);
+    } else {
+        ldr = stbi_load(filename.c_str(), &width, &height, &ncomp, 0);
+    }
+    if (!hdr && !ldr) {
+        throw std::runtime_error("cannot load image " + filename);
+    }
+}
+
+//
+// Loads an image flipping Y.
+//
+YIMG_API simage* load_image_flipy(const std::string& filename) {
+    stbi_set_flip_vertically_on_load(true);
+    auto img = load_image(filename);
+    stbi_set_flip_vertically_on_load(false);
+    return img;
+}
+
+//
+// Loads an image from memory.
+//
+YIMG_API simage* load_image_from_memory(const std::string& fmt, byte* data,
+                                        int length) {
+    auto img = new simage();
+    if (fmt == "hdr") {
+        img->hdr = stbi_loadf_from_memory(data, length, &img->width,
+                                          &img->height, &img->ncomp, 0);
+    } else {
+        img->ldr = stbi_load_from_memory(data, length, &img->width,
+                                         &img->height, &img->ncomp, 0);
+    }
+    if (!img->ldr && !img->hdr) {
+        delete img;
+        throw std::runtime_error("cannot load image from memory with format " +
+                                 fmt);
+    }
+    return img;
+}
+
+//
+// Saves an image
+//
+YIMG_API void save_image(const std::string& filename, const simage* img) {
+    save_image(filename, img->width, img->height, img->ncomp, img->hdr,
+               img->ldr);
+}
+
+//
+// Saves an image
+//
+YIMG_API void save_image(const std::string& filename, int width, int height,
+                         int ncomp, const float* hdr, const byte* ldr) {
+    if (_get_extension(filename) == ".hdr") {
+        if (!hdr) throw std::invalid_argument("hdr data required");
+        stbi_write_hdr(filename.c_str(), width, height, ncomp, hdr);
+    } else if (_get_extension(filename) == ".png") {
+        if (!ldr) throw std::invalid_argument("ldr data required");
+        stbi_write_png(filename.c_str(), width, height, ncomp, ldr,
+                       width * ncomp);
+    } else {
+        throw std::invalid_argument("unsupported output extension " + filename);
+    }
+}
+
+//
+// Removes the hdr buffer from the image.
+//
+YIMG_API float* release_hdr(simage* img) {
+    auto ptr = img->hdr;
+    img->hdr = nullptr;
+    return ptr;
+}
+
+//
+// Removes the ldr buffer from the image.
+//
+YIMG_API byte* release_ldr(simage* img) {
+    auto ptr = img->ldr;
+    img->ldr = nullptr;
+    return ptr;
+}
+
+//
+// Resize image.
+//
+YIMG_API simage* resize_image(const simage* img, int res_width,
+                              int res_height) {
+    auto res = new simage();
+    res->width = res_width;
+    res->height = res_height;
+    res->ncomp = img->ncomp;
+    resize_image(img->width, img->height, img->ncomp, img->hdr, img->ldr,
+                 res->width, res->height, res->hdr, res->ldr);
+    return res;
+}
+
+//
+// Resize image.
+//
+YIMG_API void resize_image(int width, int height, int ncomp, const float* hdr,
+                           const byte* ldr, int& res_width, int& res_height,
+                           float*& res_hdr, byte*& res_ldr) {
+    if (res_width < 0 && res_height < 0)
+        throw std::invalid_argument("at least argument should be >0");
+    if (res_width < 0)
+        res_width = (int)std::round(width * (res_height / (float)height));
+    if (res_height < 0)
+        res_height = (int)std::round(height * (res_width / (float)width));
+    if (hdr) {
+        res_hdr = new float[width * height * ncomp];
+        auto img_stride = sizeof(float) * width * ncomp;
+        auto res_stride = sizeof(float) * res_width * ncomp;
+        stbir_resize_float(hdr, width, height, img_stride, res_hdr, res_width,
+                           res_height, res_stride, ncomp);
+    } else {
+        res_ldr = new byte[width * height * ncomp];
+        auto img_stride = sizeof(byte) * width * ncomp;
+        auto res_stride = sizeof(byte) * res_width * ncomp;
+        stbir_resize_uint8_srgb(ldr, width, height, img_stride, res_ldr,
+                                res_width, res_height, res_stride, ncomp,
+                                (ncomp == 4) ? 3 : STBIR_ALPHA_CHANNEL_NONE, 0);
+    }
+}
+
+}  // namespace
