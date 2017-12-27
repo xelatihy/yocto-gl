50dcadac080b9ba3ed59431a4451fa9b8814b919
cyanpencil <lucadb96@gmail.com>
2017-12-27 17:32:23 +0100

    Rotaing torus + lighting

--- apps/myview.cpp
+++ apps/myview.cpp
@@ -27,8 +27,9 @@
 //
 
 #define YGL_OPENGL 1
-#include "../yocto/yocto_gl.h"
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "../yocto/yocto_gl.h"
 using namespace ygl;
 
 // ---------------------------------------------------------------------------
@@ -124,51 +125,147 @@ inline gl_stdsurface_program make_my_program() {
     )";
 
     string myfrag = R"(
-        #version 330 core
+#version 330 core
 
-        out vec4 fragColor;
+out vec4 fragColor;
 
-        uniform vec2 resolution;
+uniform vec2 resolution;
+uniform float time;
 
-        void main() {
-            vec2 position = gl_FragCoord.xy / resolution.xy;
-            gl_FragColor.r = 0.0;
-            gl_FragColor.g = position.x;
-            gl_FragColor.b = 0.0;
-            gl_FragColor.a = 1.0;
-            //if (gl_FragCoord.x > 100) {
-                //gl_FragColor.r = 1.0;
-            //}
-            //if (gl_FragCoord.y > 100) {
-                //gl_FragColor.g = 1.0;
-            //}
+uniform vec3 _LightDir = vec3(0,500,0);
+uniform vec3 _CameraDir = vec3(0,5,5);
+uniform float blinn_phong_alpha = 20;
+
+uniform float ka = 0.05;
+uniform float kd = 0.3;
+uniform float ks = 1.0;
+
+mat4 rotateY(float theta) {
+    return mat4(
+        vec4(cos(theta), 0, sin(theta), 0),
+        vec4(0, 1, 0, 0),
+        vec4(-sin(theta), 0, cos(theta), 0),
+        vec4(0, 0, 0, 1)
+    );
+}
+
+mat4 rotateX(float theta) {
+    return mat4(
+        vec4(1, 0, 0, 0),
+        vec4(0, cos(theta), -sin(theta), 0),
+        vec4(0, sin(theta), cos(theta), 0),
+        vec4(0, 0, 0, 1)
+    );
+}
+
+mat4 rotateZ(float theta) {
+    return mat4(
+        vec4(cos(theta), -sin(theta), 0, 0),
+        vec4(sin(theta), cos(theta), 0, 0),
+        vec4(0, 0, 1, 0),
+        vec4(0, 0, 0, 1)
+    );
+}
+
+
+// Adapted from: http://iquilezles.org/www/articles/distfunctions/distfunctions.htm
+float sdTorus(vec3 p, vec2 t)
+{
+    vec2 q = vec2(length(p.xz) - t.x, p.y);
+    return length(q) - t.y;
+}
+
+float map(vec3 p) {
+    vec3 rp = (inverse(rotateZ(time * 2)) * vec4(p, 1.0)).xyz;
+    return sdTorus(rp, vec2(1, 0.5));
+}
+
+//Adapted from: http://jamie-wong.com/2016/07/15/ray-marching-signed-distance-functions/#rotation-and-translation
+vec3 calcNormal(in vec3 p) {
+    float eps = 0.0001;
+    return normalize(vec3(
+        map(vec3(p.x + eps, p.y, p.z)) - map(vec3(p.x - eps, p.y, p.z)),
+        map(vec3(p.x, p.y + eps, p.z)) - map(vec3(p.x, p.y - eps, p.z)),
+        map(vec3(p.x, p.y, p.z + eps)) - map(vec3(p.x, p.y, p.z - eps))
+    ));
+}
+
+
+//Adapted from: http://flafla2.github.io/2016/10/01/raymarching.html
+vec4 raymarch(vec3 ro, vec3 rd) {
+    vec4 ret = vec4(0,0,0,0);
+
+    const int maxstep = 64;
+    float t = 0; // current distance traveled along ray
+    for (int i = 0; i < maxstep; ++i) {
+        vec3 p = ro + rd * t; //point hit on the surface
+        float d = map(p);   
+
+        if (d < 0.001) {
+            vec3 n = calcNormal(p);
+            float l = ka;
+            l += max(0.0, kd * dot(normalize(_LightDir - p), n));
+            vec3 h = normalize(normalize(_LightDir - p) + normalize(_CameraDir - p));
+            l += ks * pow(max(dot(n , h), 0.0), 50);
+            ret = vec4(vec3(l,l,l), 1);
+            break;
         }
-    )";
+
+        t += d;
+    }
+    return ret;
+}
+
+
+//Adapted from: https://github.com/zackpudil/raymarcher/blob/master/src/shaders/scenes/earf_day.frag
+mat3 camera(vec3 e, vec3 l) {
+    vec3 f = normalize(l - e);
+    vec3 r = cross(vec3(0, 1, 0), f);
+    vec3 u = cross(f, r);
+    
+    return mat3(r, u, f);
+}
+
+
+
+void main() {
+	vec2 uv = -1.0 + 2.0*(gl_FragCoord.xy/resolution);
+	uv.x *= resolution.x/resolution.y;
+
+	vec3 col = vec3(0);
+
+    float atime = time*0.3;
+	//vec3 ro = 2.5*vec3(cos(atime), 0, -sin(atime));
+    vec3 ro = _CameraDir;
+	vec3 rd = camera(ro, vec3(0))*normalize(vec3(uv, 2.0));
+
+	fragColor = raymarch(ro, rd);
+}    )";
 
     assert(gl_check_error());
     auto prog = gl_stdsurface_program();
-    //prog._prog = make_program(_vert_header + _vert_skinning + _vert_main,
-    //_frag_header + _frag_tonemap + _frag_lighting + _frag_brdf +
-    //_frag_material + _frag_main);
     prog._prog = make_program(myvert, myfrag);
     assert(gl_check_error());
     return prog;
 }
 
 // Display a scene
-inline void shade_scene(shade_state* st) {
-
-    // update state
+inline void shade_scene(shade_state* st, vec2i framebuffer_size) {
     if (!is_program_valid(st->prog)) st->prog = make_my_program();
 
-
-    //we need to pass just 4 vertices to the vertex shader
     bind_program(st->prog._prog);
 
+    //we need to pass just 4 vertices to the vertex shader
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     int uniform_resolution = glGetUniformLocation(st->prog._prog._pid,"resolution");
-    glUniform2f(uniform_resolution, 1000, 1000);
+    glUniform2f(uniform_resolution, framebuffer_size.x, framebuffer_size.y);
+
+    float real_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+    float time = fmod(real_time, 100000) / 1000.0f;
+    std::cout << time << std::endl;
+    int uniform_time = glGetUniformLocation(st->prog._prog._pid,"time");
+    glUniform1f(uniform_time, time);
 
     unbind_program(st->prog._prog);
 }
@@ -184,7 +281,7 @@ inline void draw(gl_window* win) {
     app->scam->aspect = aspect;
 
     gl_clear_buffers();
-    shade_scene(app->shstate);
+    shade_scene(app->shstate, framebuffer_size);
 
     if (begin_widgets(win, "yview")) {
         draw_label_widget(win, "scene", app->filename);
