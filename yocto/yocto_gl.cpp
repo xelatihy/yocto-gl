d9730d403e49a3854b17e3d5570e71173e779370
Fabio Pellacini <fabio.pellacini@gmail.com>
2017-12-29 04:23:42 +0100

    fix glew bug

--- yocto/yocto_gl.cpp
+++ yocto/yocto_gl.cpp
@@ -9998,7 +9998,7 @@ gl_window* make_window(
 
 // init gl extensions
 #ifndef __APPLE__
-    if (!glewInit()) return nullptr;
+    if (glewInit() != GLEW_OK) return nullptr;
 #endif
     return win;
 }
