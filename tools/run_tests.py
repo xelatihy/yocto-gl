1d4a67974ce1e938a6b8844c6031f5e85cb8e103
Fabio Pellacini <fabio.pellacini@gmail.com>
2018-02-18 13:06:21 +0100

    Added more tests to all apps.

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3 -b
 
-import click, os, subprocess
+import click, os, subprocess, textwrap
 
 test_dir = './tests'
 out_dir = './run_tests/out'
@@ -8,18 +8,44 @@ check_dir = './run_tests/check'
 
 quick_scenes = [
     'cornell_box.obj',
+    'basic_pl.obj',
+    'basic_al.obj',
+    'simple_pl.obj',
     'simple_al.obj',
+    'subdiv_al.obj',
+    'plastics_al.obj',
+    'plastics_el.obj',
+    'metals_al.obj',
+    'metals_el.obj',
+    'tesselation_pl.obj',
     'lines_al.gltf',
     'points_al.gltf',
-    'instancesl_pl.gltf',
+    'instancel_pl.gltf',
 ]
 
+quick_view_scenes = ( [ s.rpartition('.')[0]+'.obj'  for s in quick_scenes ] +
+                      [ s.rpartition('.')[0]+'.gltf' for s in quick_scenes ] )
+
+# def run_cmds(cmds):
+#     num = len(cmds)
+#     for idx, cmd in enumerate(cmds, 1):
+#         print(f'[CMD {idx}/{num}] {cmd}')
+#         ok = not subprocess.run(cmd, shell=True).returncode
+#         if not ok:
+#             print(f'[ERR {idx}/{num}] {cmd}')
+#         else:
+#             print(f'[OK  {idx}/{num}]', end='\r')
+
 def run_cmds(cmds):
-    num = len(cmds)
-    for idx, cmd in enumerate(cmds, 1):
-        print(f'[CMD {idx}/{num}] {cmd}')
-        ok = not subprocess.run(cmd, shell=True).returncode
-        print(f'[OK  {idx}/{num}]' if ok else f'[ERR {idx}/{num}]')
+    def shorten(cmd):
+        if not cmd: return ''
+        scmd = textwrap.wrap(cmd,width=50)[0] + ' ...'
+        scmd += ' ' * (54 - len(scmd))
+        return scmd
+    with click.progressbar(cmds, width=10, show_pos=True, show_percent=False, show_eta=False, item_show_func=shorten) as bar:
+        for cmd in bar:
+            ok = not subprocess.run(cmd, shell=True).returncode
+            if not ok: print(f'[ERR] {cmd}')
 
 @click.group()
 def run():
@@ -60,7 +86,7 @@ def qtrace(copycheck=False,testgen=False):
 @click.option('--copycheck', '-c', is_flag=True, default=False)
 @click.option('--testgen', '-t', is_flag=True, default=False)
 def qview(copycheck=False,testgen=False):
-    run_image_tests('qview', quick_scenes, './bin/yview -q --screenshot-and-exit --no-widgets -r 180', testgen, copycheck)
+    run_image_tests('qview', quick_view_scenes, './bin/yview -q --screenshot-and-exit --no-widgets -r 180', testgen, copycheck)
 
 if __name__ == '__main__':
     run()
