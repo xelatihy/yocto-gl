207fd859a354ae17622fd30ade2ded22e0cda7ec
Fabio Pellacini <fabio.pellacini@gmail.com>
2018-10-18 22:03:56 +0200

    adjusted test directories

--- scripts/test-yimproc.sh
+++ scripts/test-yimproc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x #echo on
-./bin/yimproc -o tests/run-tests/output/$2 tests/textures/$1 && ./bin/yimdiff tests/run-tests/output/$2 tests/run-tests/result/$2
+./bin/yimproc -o tests/run-tests/yimproc/output/$2 tests/textures/$1 && ./bin/yimdiff tests/run-tests/yimproc/output/$2 tests/run-tests/yimproc/result/$2
