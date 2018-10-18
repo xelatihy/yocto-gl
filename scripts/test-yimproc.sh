74b7d13a5dea5374658c22e9a247883e3e411673
Fabio Pellacini <fabio.pellacini@gmail.com>
2018-10-18 22:40:59 +0200

    setup tests

--- scripts/test-yimproc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -x #echo on
-./bin/yimproc -o tests/run-tests/yimproc/output/$2 tests/textures/$1 && ./bin/yimdiff tests/run-tests/yimproc/output/$2 tests/run-tests/yimproc/result/$2
--- scripts/test-ytrace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -x #echo on
-./bin/ytrace -o tests/run-tests/ytrace/output/$2 tests/textures/$1 && ./bin/yimdiff tests/run-tests/ytrace/output/$2 tests/run-tests/ytrace/result/$2
