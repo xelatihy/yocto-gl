dd6d7a69103518adecebbf680562b92e8a7c43fc
Fabio Pellacini <fabio.pellacini@gmail.com>
2018-10-24 06:25:39 +0200

    No surface tesselation (#149)
    Surfaces have been fully removed from the system. There are several reasons for it but it is mostly a matter of code simplification and reducing code paths. 
    
    We still need to figure out a proper manner of doing IO. In particular, whether we should be explicit about maintaining face varying data or whether we should obey the convention of the file format (load all glTF as vertex-varying and all OBJ as face varying no matter the content). Also the shape io code is getting complicated enough that it is probably best to use intermediate representation for loading/saving mesh data (like using directly yocto_shape or make_shape_data).

--- apps/yview.cpp
+++ apps/yview.cpp
@@ -35,7 +35,8 @@ struct glshape {
     glarraybuffer gl_pos = {}, gl_norm = {}, gl_texcoord = {}, gl_color = {},
                   gl_tangsp   = {};
     glelementbuffer gl_points = {}, gl_lines = {}, gl_triangles = {},
-                    gl_quads = {};
+                    gl_quads  = {};
+    int num_facevarying_quads = 0;
 };
 
 struct draw_glstate {
@@ -522,6 +523,10 @@ void draw_glshape(draw_glstate* state, const yocto_shape* shape,
         set_gluniform(state->prog, "elem_type", 3);
         draw_gltriangles(vbos.gl_quads, shape->quads.size() * 2);
     }
+    if (!shape->quads_positions.empty()) {
+        set_gluniform(state->prog, "elem_type", 3);
+        draw_gltriangles(vbos.gl_quads, vbos.num_facevarying_quads * 2);
+    }
 
 #if 0
     if ((vbos.gl_edges && edges && !wireframe) || highlighted) {
@@ -641,6 +646,7 @@ draw_glstate* init_draw_state(glwindow* win) {
         }
     }
     for (auto& shape : app->scene->shapes) {
+        if (!shape->quads_positions.empty()) continue;
         auto vbos = glshape();
         if (!shape->positions.empty())
             vbos.gl_pos = make_glarraybuffer(shape->positions, false);
@@ -662,6 +668,30 @@ draw_glstate* init_draw_state(glwindow* win) {
             vbos.gl_quads = make_glelementbuffer(
                 convert_quads_to_triangles(shape->quads), false);
         state->shps[shape] = vbos;
+    }
+    for (auto& shape : app->scene->shapes) {
+        if (shape->quads_positions.empty()) continue;
+        auto vbos          = glshape();
+        auto quads         = vector<vec4i>();
+        auto positions     = vector<vec3f>();
+        auto normals       = vector<vec3f>();
+        auto colors        = vector<vec4f>();
+        auto texturecoords = vector<vec2f>();
+        convert_face_varying(quads, positions, normals, texturecoords, colors,
+            shape->quads_positions, shape->quads_normals,
+            shape->quads_texturecoords, shape->quads_colors, shape->positions,
+            shape->normals, shape->texturecoords, shape->colors);
+        if (!positions.empty())
+            vbos.gl_pos = make_glarraybuffer(positions, false);
+        if (!normals.empty()) vbos.gl_norm = make_glarraybuffer(normals, false);
+        if (!texturecoords.empty())
+            vbos.gl_texcoord = make_glarraybuffer(texturecoords, false);
+        if (!colors.empty()) vbos.gl_color = make_glarraybuffer(colors, false);
+        if (!quads.empty())
+            vbos.gl_quads = make_glelementbuffer(
+                convert_quads_to_triangles(quads), false);
+        vbos.num_facevarying_quads = (int)quads.size();
+        state->shps[shape]         = vbos;
     }
     return state;
 }
@@ -800,7 +830,7 @@ int main(int argc, char* argv[]) {
 
     // tesselate
     if (!quiet) log_info("tesselating scene elements\n");
-    tesselate_subdivs(app->scene.get());
+    tesselate_shapes(app->scene.get());
 
     // add components
     if (!quiet) log_info("adding scene elements\n");
