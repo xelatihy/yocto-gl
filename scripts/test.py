3bc1ef5106361e72d1f74fb054e59f6e1d0e7d5b
Fabio Pellacini <fabio.pellacini@gmail.com>
2018-11-21 07:42:00 +0100

    move test directory (#289)

--- scripts/test.py
+++ scripts/test.py
@@ -9,15 +9,15 @@ def cli():
 @cli.command()
 def run():
     os.system('mkdir -p build && mkdir -p build/release && cd build/release && cmake ../.. -GNinja -DYOCTO_EMBREE=ON')
-    os.system('rm tests/run-tests/output/*.png; rm tests/run-tests/difference/*.png')
+    os.system('rm runtests/output/*.png; rm  runtests/difference/*.png')
     os.system('cd build/release && ctest -j 4 --output-on-failure')
 
 @cli.command()
 def clean():
-    os.system('rm tests/run-tests/output/*.png; rm tests/run-tests/difference/*.png')
+    os.system('rm runtests/output/*.png; rm runtests/difference/*.png')
 
 @cli.command()
 def update():
-    os.system('cp tests/run-tests/output/*.png tests/run-tests/result/')
+    os.system('cp runtests/output/*.png runtests/result/')
 
 cli()
