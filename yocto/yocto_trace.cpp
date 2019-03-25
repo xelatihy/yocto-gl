b601c6f5de6b378ad5a3f23c5fc686eaa04f067a
giacomo <nazzarogiacomo@gmail.com>
2019-03-25 11:27:10 +0100

    fix delta refraction

--- yocto/yocto_trace.cpp
+++ yocto/yocto_trace.cpp
@@ -687,11 +687,8 @@ vec3f sample_brdf_direction(const microfacet_brdf& brdf, const vec3f& normal_,
 
 // Picks a direction based on the BRDF
 vec3f sample_delta_brdf_direction(const microfacet_brdf& brdf,
-    const vec3f& normal_, const vec3f& outgoing, float rnl, const vec2f& rn) {
+    const vec3f& normal, const vec3f& outgoing, float rnl, const vec2f& rn) {
     if (!is_brdf_delta(brdf)) return zero3f;
-
-    // flip normal if necessary
-    auto normal = (dot(normal_, outgoing) > 0) ? normal_ : -normal_;
 
     // orientation
     auto outgoing_up = dot(outgoing, normal) > 0;
