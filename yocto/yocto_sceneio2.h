4dad44d6d5e7c8e9b1e8c0c43d606ab60d12b531
Fabio Pellacini <fabio.pellacini@gmail.com>
2019-10-09 09:25:00 +0200

    updated

--- yocto/yocto_sceneio2.h
+++ yocto/yocto_sceneio2.h
@@ -39,14 +39,16 @@
 // SOFTWARE.
 //
 
-#ifndef _YOCTO_SCENEIO_H_
-#define _YOCTO_SCENEIO_H_
+#ifndef _YOCTO_SCENEIO2_H_
+#define _YOCTO_SCENEIO2_H_
 
 // -----------------------------------------------------------------------------
 // INCLUDES
 // -----------------------------------------------------------------------------
 
-#include "yocto_scene.h"
+#include "yocto_math.h"
+#include "yocto_common.h"
+#include "yocto_image.h"
 
 // -----------------------------------------------------------------------------
 // SCENE DATA
@@ -256,32 +258,9 @@ struct save_params {
 
 // Load/save a scene in the supported formats.
 void load_scene(
-    const string& filename, yocto_scene& scene, const load_params& params = {});
-void save_scene(const string& filename, const yocto_scene& scene,
-    const save_params& params = {});
-
-// Load/save a scene in the supported formats.
-void load_scene(
     const string& filename, scene_model& scene, const load_params& params = {});
 void save_scene(const string& filename, const scene_model& scene,
     const save_params& params = {});
-
-
-// Load/save scene textures
-void load_texture(yocto_texture& texture, const string& dirname);
-void save_texture(const yocto_texture& texture, const string& dirname);
-void load_textures(
-    yocto_scene& scene, const string& dirname, const load_params& params);
-void save_textures(
-    const yocto_scene& scene, const string& dirname, const save_params& params);
-
-// Load/save scene shapes
-void load_shape(yocto_shape& shape, const string& dirname);
-void save_shape(const yocto_shape& shape, const string& dirname);
-void load_shapes(
-    yocto_scene& scene, const string& dirname, const load_params& params);
-void save_shapes(
-    const yocto_scene& scene, const string& dirname, const save_params& params);
 
 }  // namespace yocto
 
