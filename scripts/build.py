0172eede206eed784ba28b97a5a3622150f22eef
Fabio Pellacini <fabio.pellacini@gmail.com>
2019-11-21 22:03:01 +0100

    fixing build script for using multiple paths

--- scripts/build.py
+++ scripts/build.py
@@ -8,41 +8,28 @@ def cli():
 
 @cli.command()
 def release():
-    os.system('mkdir -p build && mkdir -p build/release && cd build/release && cmake ../.. -GNinja -DYOCTO_EMBREE=ON && ninja')
-
-@cli.command()
-def noembree():
-    os.system('mkdir -p build && mkdir -p build/release && cd build/release && cmake ../.. -GNinja -DYOCTO_EMBREE=OFF && ninja')
+    os.makedirs('build/terminal/Release', exist_ok=True)
+    os.chdir('build/terminal/Release')
+    os.system('cmake ../../.. -GNinja -DCMAKE_BUILD_TYPE=Release -DYOCTO_EMBREE=ON')
+    os.system('cmake --build . --parallel 8')
 
 @cli.command()
 def debug():
-    os.system('mkdir -p build && mkdir -p build/debug && cd build/debug && cmake ../.. -GNinja -DYOCTO_EMBREE=ON -DCMAKE_BUILD_TYPE=Debug && ninja')
+    os.makedirs('build/terminal/Debug', exist_ok=True)
+    os.chdir('build/terminal/Debug')
+    os.system('cmake ../../.. -GNinja -DCMAKE_BUILD_TYPE=Debug -DYOCTO_EMBREE=ON')
+    os.system('cmake --build . --parallel 8')
 
 @cli.command()
 def xcode():
-    os.system('mkdir -p build && mkdir -p build/xcode && cd build/xcode && cmake ../.. -GXcode -DYOCTO_EMBREE=ON && open yocto-gl.xcodeproj')
+    os.makedirs('build/xcode', exist_ok=True)
+    os.chdir('build/xcode')
+    os.system('cmake ../.. -GXcode -DYOCTO_EMBREE=ON')
+    os.system('open yocto-gl.xcodeproj')
 
 @cli.command()
 def clean():
     os.system('rm -rf bin && rm -rf build')
-
-@cli.command()
-def tidy():
-    os.system('/usr/local/opt/llvm/bin/clang-tidy -checks="-* readability-*" yocto/*.cpp -- -std=c++17 -I./yocto -I/usr/local/include -I/opt/local/include -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1 -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/include -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include')
-
-@cli.command()
-def tests():
-    for ext in ['obj', 'gltf', 'json', 'ybin', 'pbrt']:
-        os.system(f'rm -rf tests/{ext}; mkdir tests/{ext}')
-    for filename in glob.glob('tests/*.json'):
-        print(filename)
-        basename = os.path.basename(filename).replace('.json','')
-        for ext in ['obj', 'gltf', 'json', 'ybin', 'pbrt']:
-            os.system(f'mkdir -p tests/{ext}/{basename}')
-            os.system(f'mkdir -p tests/{ext}/{basename}/textures')
-            os.system(f'mkdir -p tests/{ext}/{basename}/meshes')
-            os.system(f'mkdir -p tests/{ext}/{basename}/models')
-            os.system(f'./bin/yscnproc -o tests/{ext}/{basename}/{basename}.{ext} {filename}')
 
 @cli.command()
 def format():
