7556f01ac40902fcc807e4ceba1bb992873c1c40
Fabio Pellacini <fabio.pellacini@gmail.com>
2020-07-02 12:07:46 +0200

    updated

--- libs/yocto/yocto_noise.h
+++ libs/yocto/yocto_noise.h
@@ -143,6 +143,34 @@ inline const unsigned char p[512] = {
     // clang-format on
 };
 
+inline float perlin_noise(float x, int wx) {
+  auto ease   = [](float a) { return ((a * 6 - 15) * a + 10) * a * a * a; };
+  auto ifloor = [](float a) -> int {
+    int ai = (int)a;
+    return (a < ai) ? ai - 1 : ai;
+  };
+  auto grad = [](int hash, float x) -> float {
+    auto h    = hash & 15;
+    auto grad = 1.0f + (h & 7);  // Gradient value 1.0, 2.0, ..., 8.0
+    if (h & 8) grad = -grad;     // and a random sign for the gradient
+    return (grad * x);           // Multiply the gradient with the distance
+  };
+  auto hash = [](int x) -> int { return p[x]; };
+
+  uint mx = (wx - 1) & 255;
+  auto ix = ifloor(x);
+  auto fx = x - ix;
+  auto x0 = ix & mx;
+  auto x1 = (ix + 1) & mx;
+
+  auto ux = ease(fx);
+
+  auto n0 = grad(hash(x0), fx);
+  auto n1 = grad(hash(x1), fx - 1);
+
+  return lerp(n0, n1, ux);
+}
+
 inline float perlin_noise(float x, float y, int wx, int wy) {
   auto ease   = [](float a) { return ((a * 6 - 15) * a + 10) * a * a * a; };
   auto ifloor = [](float a) -> int {
@@ -207,6 +235,71 @@ inline float perlin_noise(float x, float y, float z, int wx, int wy, int wz) {
   auto n101 = grad(hash(x1, y0, z1), fx - 1, fy, fz - 1);
   auto n110 = grad(hash(x1, y1, z0), fx - 1, fy - 1, fz);
   auto n111 = grad(hash(x1, y1, z1), fx - 1, fy - 1, fz - 1);
+
+  auto n00 = lerp(n000, n001, uz);
+  auto n01 = lerp(n010, n011, uz);
+  auto n10 = lerp(n100, n101, uz);
+  auto n11 = lerp(n110, n111, uz);
+
+  auto n0 = lerp(n00, n01, uy);
+  auto n1 = lerp(n10, n11, uy);
+
+  return lerp(n0, n1, ux);
+}
+
+inline float perlin_noise(
+    float x, float y, float z, float w, int wx, int wy, int wz, int ww) {
+  auto ease   = [](float a) { return ((a * 6 - 15) * a + 10) * a * a * a; };
+  auto ifloor = [](float a) -> int {
+    int ai = (int)a;
+    return (a < ai) ? ai - 1 : ai;
+  };
+  auto grad = [](int hash, float x, float y, float z, float t) -> float {
+    auto h = hash & 31;       // Convert low 5 bits of hash code into 32 simple
+    auto u = h < 24 ? x : y;  // gradient directions, and compute dot product.
+    auto v = h < 16 ? y : z;
+    auto w = h < 8 ? z : t;
+    return ((h & 1) ? -u : u) + ((h & 2) ? -v : v) + ((h & 4) ? -w : w);
+  };
+  auto hash = [](int x, int y, int z, int w) -> int {
+    return p[p[p[p[x] + y] + z] + w];
+  };
+
+  uint mx = (wx - 1) & 255, my = (wy - 1) & 255, mz = (wz - 1) & 255,
+       mw = (ww - 1) & 255;
+  auto ix = ifloor(x), iy = ifloor(y), iz = ifloor(z), iw = ifloor(w);
+  auto fx = x - ix, fy = y - iy, fz = z - iz, fw = w - iw;
+  auto x0 = ix & mx, y0 = iy & my, z0 = iz & mz, w0 = iw & mw;
+  auto x1 = (ix + 1) & mx, y1 = (iy + 1) & my, z1 = (iz + 1) & mz,
+       w1 = (iw + 1) & mw;
+
+  auto ux = ease(fx), uy = ease(fy), uz = ease(fz), uw = ease(fw);
+
+  auto n0000 = grad(hash(x0, y0, z0, w0), fx, fy, fz, fw);
+  auto n0001 = grad(hash(x0, y0, z0, w1), fx, fy, fz, fw - 1);
+  auto n0010 = grad(hash(x0, y0, z1, w0), fx, fy, fz - 1, fw);
+  auto n0011 = grad(hash(x0, y0, z1, w1), fx, fy, fz - 1, fw - 1);
+  auto n0100 = grad(hash(x0, y1, z0, w0), fx, fy - 1, fz, fw);
+  auto n0101 = grad(hash(x0, y1, z0, w1), fx, fy - 1, fz, fw - 1);
+  auto n0110 = grad(hash(x0, y1, z1, w0), fx, fy - 1, fz - 1, fw);
+  auto n0111 = grad(hash(x0, y1, z1, w1), fx, fy - 1, fz - 1, fw - 1);
+  auto n1000 = grad(hash(x1, y0, z0, w0), fx - 1, fy, fz, fw);
+  auto n1001 = grad(hash(x1, y0, z0, w1), fx - 1, fy, fz, fw - 1);
+  auto n1010 = grad(hash(x1, y0, z1, w0), fx - 1, fy, fz - 1, fw);
+  auto n1011 = grad(hash(x1, y0, z1, w1), fx - 1, fy, fz - 1, fw - 1);
+  auto n1100 = grad(hash(x1, y1, z0, w0), fx - 1, fy - 1, fz, fw);
+  auto n1101 = grad(hash(x1, y1, z0, w1), fx - 1, fy - 1, fz, fw - 1);
+  auto n1110 = grad(hash(x1, y1, z1, w0), fx - 1, fy - 1, fz - 1, fw);
+  auto n1111 = grad(hash(x1, y1, z1, w1), fx - 1, fy - 1, fz - 1, fw - 1);
+
+  auto n000 = lerp(n0000, n0001, uw);
+  auto n001 = lerp(n0010, n0011, uw);
+  auto n010 = lerp(n0100, n0101, uw);
+  auto n011 = lerp(n0110, n0111, uw);
+  auto n100 = lerp(n1000, n1001, uw);
+  auto n101 = lerp(n1010, n1011, uw);
+  auto n110 = lerp(n1100, n1101, uw);
+  auto n111 = lerp(n1110, n1111, uw);
 
   auto n00 = lerp(n000, n001, uz);
   auto n01 = lerp(n010, n011, uz);
