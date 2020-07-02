55bc84fdc5974bc1bdcc8e32df9e3058b335b362
Fabio Pellacini <fabio.pellacini@gmail.com>
2020-07-02 10:28:56 +0200

    updated

--- libs/yocto/yocto_noise.h
+++ libs/yocto/yocto_noise.h
@@ -102,227 +102,254 @@ inline float perlin_turbulence(const vec3f& p, float lacunarity = 2,
 // -----------------------------------------------------------------------------
 namespace yocto {
 
-// clang-format off
-inline float _stb__perlin_lerp(float a, float b, float t)
-{
-   return a + (b-a) * t;
+// not same permutation table as Perlin's reference to avoid copyright issues;
+// Perlin's table can be found at http://mrl.nyu.edu/~perlin/noise/
+// @OPTIMIZE: should this be unsigned char instead of int for cache?
+inline const unsigned char p[512] = {
+    // clang-format off
+   23, 125, 161, 52, 103, 117, 70, 37, 247, 101, 203, 169, 124, 126, 44, 123,
+   152, 238, 145, 45, 171, 114, 253, 10, 192, 136, 4, 157, 249, 30, 35, 72,
+   175, 63, 77, 90, 181, 16, 96, 111, 133, 104, 75, 162, 93, 56, 66, 240,
+   8, 50, 84, 229, 49, 210, 173, 239, 141, 1, 87, 18, 2, 198, 143, 57,
+   225, 160, 58, 217, 168, 206, 245, 204, 199, 6, 73, 60, 20, 230, 211, 233,
+   94, 200, 88, 9, 74, 155, 33, 15, 219, 130, 226, 202, 83, 236, 42, 172,
+   165, 218, 55, 222, 46, 107, 98, 154, 109, 67, 196, 178, 127, 158, 13, 243,
+   65, 79, 166, 248, 25, 224, 115, 80, 68, 51, 184, 128, 232, 208, 151, 122,
+   26, 212, 105, 43, 179, 213, 235, 148, 146, 89, 14, 195, 28, 78, 112, 76,
+   250, 47, 24, 251, 140, 108, 186, 190, 228, 170, 183, 139, 39, 188, 244, 246,
+   132, 48, 119, 144, 180, 138, 134, 193, 82, 182, 120, 121, 86, 220, 209, 3,
+   91, 241, 149, 85, 205, 150, 113, 216, 31, 100, 41, 164, 177, 214, 153, 231,
+   38, 71, 185, 174, 97, 201, 29, 95, 7, 92, 54, 254, 191, 118, 34, 221,
+   131, 11, 163, 99, 234, 81, 227, 147, 156, 176, 17, 142, 69, 12, 110, 62,
+   27, 255, 0, 194, 59, 116, 242, 252, 19, 21, 187, 53, 207, 129, 64, 135,
+   61, 40, 167, 237, 102, 223, 106, 159, 197, 189, 215, 137, 36, 32, 22, 5,
+   // and a second copy so we don't need an extra mask or static initializer
+   23, 125, 161, 52, 103, 117, 70, 37, 247, 101, 203, 169, 124, 126, 44, 123,
+   152, 238, 145, 45, 171, 114, 253, 10, 192, 136, 4, 157, 249, 30, 35, 72,
+   175, 63, 77, 90, 181, 16, 96, 111, 133, 104, 75, 162, 93, 56, 66, 240,
+   8, 50, 84, 229, 49, 210, 173, 239, 141, 1, 87, 18, 2, 198, 143, 57,
+   225, 160, 58, 217, 168, 206, 245, 204, 199, 6, 73, 60, 20, 230, 211, 233,
+   94, 200, 88, 9, 74, 155, 33, 15, 219, 130, 226, 202, 83, 236, 42, 172,
+   165, 218, 55, 222, 46, 107, 98, 154, 109, 67, 196, 178, 127, 158, 13, 243,
+   65, 79, 166, 248, 25, 224, 115, 80, 68, 51, 184, 128, 232, 208, 151, 122,
+   26, 212, 105, 43, 179, 213, 235, 148, 146, 89, 14, 195, 28, 78, 112, 76,
+   250, 47, 24, 251, 140, 108, 186, 190, 228, 170, 183, 139, 39, 188, 244, 246,
+   132, 48, 119, 144, 180, 138, 134, 193, 82, 182, 120, 121, 86, 220, 209, 3,
+   91, 241, 149, 85, 205, 150, 113, 216, 31, 100, 41, 164, 177, 214, 153, 231,
+   38, 71, 185, 174, 97, 201, 29, 95, 7, 92, 54, 254, 191, 118, 34, 221,
+   131, 11, 163, 99, 234, 81, 227, 147, 156, 176, 17, 142, 69, 12, 110, 62,
+   27, 255, 0, 194, 59, 116, 242, 252, 19, 21, 187, 53, 207, 129, 64, 135,
+   61, 40, 167, 237, 102, 223, 106, 159, 197, 189, 215, 137, 36, 32, 22, 5,
+    // clang-format on
+};
+
+inline float _stb__perlin_lerp(float a, float b, float t) {
+  return a + (b - a) * t;
 }
 
-inline int _stb__perlin_fastfloor(float a)
-{
-    int ai = (int) a;
-    return (a < ai) ? ai-1 : ai;
+inline int _stb__perlin_fastfloor(float a) {
+  int ai = (int)a;
+  return (a < ai) ? ai - 1 : ai;
 }
 
 // different grad function from Perlin's, but easy to modify to match reference
-inline float _stb__perlin_grad(int hash, float x, float y, float z)
-{
-   static float basis[12][4] =
-   {
-      {  1, 1, 0 },
-      { -1, 1, 0 },
-      {  1,-1, 0 },
-      { -1,-1, 0 },
-      {  1, 0, 1 },
-      { -1, 0, 1 },
-      {  1, 0,-1 },
-      { -1, 0,-1 },
-      {  0, 1, 1 },
-      {  0,-1, 1 },
-      {  0, 1,-1 },
-      {  0,-1,-1 },
-   };
+inline float _stb__perlin_grad(int hash, float x, float y, float z) {
+  static float basis[12][4] = {
+      {1, 1, 0},
+      {-1, 1, 0},
+      {1, -1, 0},
+      {-1, -1, 0},
+      {1, 0, 1},
+      {-1, 0, 1},
+      {1, 0, -1},
+      {-1, 0, -1},
+      {0, 1, 1},
+      {0, -1, 1},
+      {0, 1, -1},
+      {0, -1, -1},
+  };
 
-   // perlin's gradient has 12 cases so some get used 1/16th of the time
-   // and some 2/16ths. We reduce bias by changing those fractions
-   // to 5/64ths and 6/64ths, and the same 4 cases get the extra weight.
-   static unsigned char indices[64] =
-   {
-      0,1,2,3,4,5,6,7,8,9,10,11,
-      0,9,1,11,
-      0,1,2,3,4,5,6,7,8,9,10,11,
-      0,1,2,3,4,5,6,7,8,9,10,11,
-      0,1,2,3,4,5,6,7,8,9,10,11,
-      0,1,2,3,4,5,6,7,8,9,10,11,
-   };
+  // perlin's gradient has 12 cases so some get used 1/16th of the time
+  // and some 2/16ths. We reduce bias by changing those fractions
+  // to 5/64ths and 6/64ths, and the same 4 cases get the extra weight.
+  static unsigned char indices[64] = {
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      0,
+      9,
+      1,
+      11,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+  };
 
-   // if you use reference permutation table, change 63 below to 15 to match reference
-   // (this is why the ordering of the table above is funky)
-   float *grad = basis[indices[hash & 63]];
-   return grad[0]*x + grad[1]*y + grad[2]*z;
+  // if you use reference permutation table, change 63 below to 15 to match
+  // reference (this is why the ordering of the table above is funky)
+  float* grad = basis[indices[hash & 63]];
+  return grad[0] * x + grad[1] * y + grad[2] * z;
 }
 
-inline float _stb_perlin_noise3(float x, float y, float z, int x_wrap, int y_wrap, int z_wrap)
-{
-    // not same permutation table as Perlin's reference to avoid copyright issues;
-    // Perlin's table can be found at http://mrl.nyu.edu/~perlin/noise/
-    // @OPTIMIZE: should this be unsigned char instead of int for cache?
-    static unsigned char _stb__perlin_randtab[512] =
-    {
-    23, 125, 161, 52, 103, 117, 70, 37, 247, 101, 203, 169, 124, 126, 44, 123,
-    152, 238, 145, 45, 171, 114, 253, 10, 192, 136, 4, 157, 249, 30, 35, 72,
-    175, 63, 77, 90, 181, 16, 96, 111, 133, 104, 75, 162, 93, 56, 66, 240,
-    8, 50, 84, 229, 49, 210, 173, 239, 141, 1, 87, 18, 2, 198, 143, 57,
-    225, 160, 58, 217, 168, 206, 245, 204, 199, 6, 73, 60, 20, 230, 211, 233,
-    94, 200, 88, 9, 74, 155, 33, 15, 219, 130, 226, 202, 83, 236, 42, 172,
-    165, 218, 55, 222, 46, 107, 98, 154, 109, 67, 196, 178, 127, 158, 13, 243,
-    65, 79, 166, 248, 25, 224, 115, 80, 68, 51, 184, 128, 232, 208, 151, 122,
-    26, 212, 105, 43, 179, 213, 235, 148, 146, 89, 14, 195, 28, 78, 112, 76,
-    250, 47, 24, 251, 140, 108, 186, 190, 228, 170, 183, 139, 39, 188, 244, 246,
-    132, 48, 119, 144, 180, 138, 134, 193, 82, 182, 120, 121, 86, 220, 209, 3,
-    91, 241, 149, 85, 205, 150, 113, 216, 31, 100, 41, 164, 177, 214, 153, 231,
-    38, 71, 185, 174, 97, 201, 29, 95, 7, 92, 54, 254, 191, 118, 34, 221,
-    131, 11, 163, 99, 234, 81, 227, 147, 156, 176, 17, 142, 69, 12, 110, 62,
-    27, 255, 0, 194, 59, 116, 242, 252, 19, 21, 187, 53, 207, 129, 64, 135,
-    61, 40, 167, 237, 102, 223, 106, 159, 197, 189, 215, 137, 36, 32, 22, 5,
+inline float _stb_perlin_noise3(
+    float x, float y, float z, int x_wrap, int y_wrap, int z_wrap) {
+  auto ease   = [](float a) { return ((a * 6 - 15) * a + 10) * a * a * a; };
+  auto ifloor = [](float a) -> int {
+    int ai = (int)a;
+    return (a < ai) ? ai - 1 : ai;
+  };
 
-    // and a second copy so we don't need an extra mask or static initializer
-    23, 125, 161, 52, 103, 117, 70, 37, 247, 101, 203, 169, 124, 126, 44, 123,
-    152, 238, 145, 45, 171, 114, 253, 10, 192, 136, 4, 157, 249, 30, 35, 72,
-    175, 63, 77, 90, 181, 16, 96, 111, 133, 104, 75, 162, 93, 56, 66, 240,
-    8, 50, 84, 229, 49, 210, 173, 239, 141, 1, 87, 18, 2, 198, 143, 57,
-    225, 160, 58, 217, 168, 206, 245, 204, 199, 6, 73, 60, 20, 230, 211, 233,
-    94, 200, 88, 9, 74, 155, 33, 15, 219, 130, 226, 202, 83, 236, 42, 172,
-    165, 218, 55, 222, 46, 107, 98, 154, 109, 67, 196, 178, 127, 158, 13, 243,
-    65, 79, 166, 248, 25, 224, 115, 80, 68, 51, 184, 128, 232, 208, 151, 122,
-    26, 212, 105, 43, 179, 213, 235, 148, 146, 89, 14, 195, 28, 78, 112, 76,
-    250, 47, 24, 251, 140, 108, 186, 190, 228, 170, 183, 139, 39, 188, 244, 246,
-    132, 48, 119, 144, 180, 138, 134, 193, 82, 182, 120, 121, 86, 220, 209, 3,
-    91, 241, 149, 85, 205, 150, 113, 216, 31, 100, 41, 164, 177, 214, 153, 231,
-    38, 71, 185, 174, 97, 201, 29, 95, 7, 92, 54, 254, 191, 118, 34, 221,
-    131, 11, 163, 99, 234, 81, 227, 147, 156, 176, 17, 142, 69, 12, 110, 62,
-    27, 255, 0, 194, 59, 116, 242, 252, 19, 21, 187, 53, 207, 129, 64, 135,
-    61, 40, 167, 237, 102, 223, 106, 159, 197, 189, 215, 137, 36, 32, 22, 5,
-    };
+  unsigned int mx = (x_wrap - 1) & 255;
+  unsigned int my = (y_wrap - 1) & 255;
+  unsigned int mz = (z_wrap - 1) & 255;
+  auto         px = ifloor(x), py = ifloor(y), pz = ifloor(z);
+  auto         x0 = px & mx, x1 = (px + 1) & mx;
+  auto         y0 = py & my, y1 = (py + 1) & my;
+  auto         z0 = pz & mz, z1 = (pz + 1) & mz;
+  int          r0, r1, r00, r01, r10, r11;
 
-   float u,v,w;
-   float n000,n001,n010,n011,n100,n101,n110,n111;
-   float n00,n01,n10,n11;
-   float n0,n1;
+  x -= px;
+  auto u = ease(x);
+  y -= py;
+  auto v = ease(y);
+  z -= pz;
+  auto w = ease(z);
 
-   unsigned int x_mask = (x_wrap-1) & 255;
-   unsigned int y_mask = (y_wrap-1) & 255;
-   unsigned int z_mask = (z_wrap-1) & 255;
-   int px = _stb__perlin_fastfloor(x);
-   int py = _stb__perlin_fastfloor(y);
-   int pz = _stb__perlin_fastfloor(z);
-   int x0 = px & x_mask, x1 = (px+1) & x_mask;
-   int y0 = py & y_mask, y1 = (py+1) & y_mask;
-   int z0 = pz & z_mask, z1 = (pz+1) & z_mask;
-   int r0,r1, r00,r01,r10,r11;
+  r0 = p[x0];
+  r1 = p[x1];
 
-   #define _stb__perlin_ease(a)   (((a*6-15)*a + 10) * a * a * a)
+  r00 = p[r0 + y0];
+  r01 = p[r0 + y1];
+  r10 = p[r1 + y0];
+  r11 = p[r1 + y1];
 
-   x -= px; u = _stb__perlin_ease(x);
-   y -= py; v = _stb__perlin_ease(y);
-   z -= pz; w = _stb__perlin_ease(z);
+  auto n000 = _stb__perlin_grad(p[r00 + z0], x, y, z);
+  auto n001 = _stb__perlin_grad(p[r00 + z1], x, y, z - 1);
+  auto n010 = _stb__perlin_grad(p[r01 + z0], x, y - 1, z);
+  auto n011 = _stb__perlin_grad(p[r01 + z1], x, y - 1, z - 1);
+  auto n100 = _stb__perlin_grad(p[r10 + z0], x - 1, y, z);
+  auto n101 = _stb__perlin_grad(p[r10 + z1], x - 1, y, z - 1);
+  auto n110 = _stb__perlin_grad(p[r11 + z0], x - 1, y - 1, z);
+  auto n111 = _stb__perlin_grad(p[r11 + z1], x - 1, y - 1, z - 1);
 
-   r0 = _stb__perlin_randtab[x0];
-   r1 = _stb__perlin_randtab[x1];
+  auto n00 = lerp(n000, n001, w);
+  auto n01 = lerp(n010, n011, w);
+  auto n10 = lerp(n100, n101, w);
+  auto n11 = lerp(n110, n111, w);
 
-   r00 = _stb__perlin_randtab[r0+y0];
-   r01 = _stb__perlin_randtab[r0+y1];
-   r10 = _stb__perlin_randtab[r1+y0];
-   r11 = _stb__perlin_randtab[r1+y1];
+  auto n0 = lerp(n00, n01, v);
+  auto n1 = lerp(n10, n11, v);
 
-   n000 = _stb__perlin_grad(_stb__perlin_randtab[r00+z0], x  , y  , z   );
-   n001 = _stb__perlin_grad(_stb__perlin_randtab[r00+z1], x  , y  , z-1 );
-   n010 = _stb__perlin_grad(_stb__perlin_randtab[r01+z0], x  , y-1, z   );
-   n011 = _stb__perlin_grad(_stb__perlin_randtab[r01+z1], x  , y-1, z-1 );
-   n100 = _stb__perlin_grad(_stb__perlin_randtab[r10+z0], x-1, y  , z   );
-   n101 = _stb__perlin_grad(_stb__perlin_randtab[r10+z1], x-1, y  , z-1 );
-   n110 = _stb__perlin_grad(_stb__perlin_randtab[r11+z0], x-1, y-1, z   );
-   n111 = _stb__perlin_grad(_stb__perlin_randtab[r11+z1], x-1, y-1, z-1 );
-
-   n00 = _stb__perlin_lerp(n000,n001,w);
-   n01 = _stb__perlin_lerp(n010,n011,w);
-   n10 = _stb__perlin_lerp(n100,n101,w);
-   n11 = _stb__perlin_lerp(n110,n111,w);
-
-   n0 = _stb__perlin_lerp(n00,n01,v);
-   n1 = _stb__perlin_lerp(n10,n11,v);
-
-   return _stb__perlin_lerp(n0,n1,u);
+  return lerp(n0, n1, u);
 }
-
-inline float _stb_perlin_ridge_noise3(float x, float y, float z,float lacunarity, float gain, float offset, int octaves,int x_wrap, int y_wrap, int z_wrap)
-{
-   int i;
-   float frequency = 1.0f;
-   float prev = 1.0f;
-   float amplitude = 0.5f;
-   float sum = 0.0f;
-
-   for (i = 0; i < octaves; i++) {
-      float r = (float)(_stb_perlin_noise3(x*frequency,y*frequency,z*frequency,x_wrap,y_wrap,z_wrap));
-      r = r<0 ? -r : r; // fabs()
-      r = offset - r;
-      r = r*r;
-      sum += r*amplitude*prev;
-      prev = r;
-      frequency *= lacunarity;
-      amplitude *= gain;
-   }
-   return sum;
-}
-
-inline float _stb_perlin_fbm_noise3(float x, float y, float z,float lacunarity, float gain, int octaves,int x_wrap, int y_wrap, int z_wrap)
-{
-   int i;
-   float frequency = 1.0f;
-   float amplitude = 1.0f;
-   float sum = 0.0f;
-
-   for (i = 0; i < octaves; i++) {
-      sum += _stb_perlin_noise3(x*frequency,y*frequency,z*frequency,x_wrap,y_wrap,z_wrap)*amplitude;
-      frequency *= lacunarity;
-      amplitude *= gain;
-   }
-   return sum;
-}
-
-inline float _stb_perlin_turbulence_noise3(float x, float y, float z, float lacunarity, float gain, int octaves,int x_wrap, int y_wrap, int z_wrap)
-{
-   int i;
-   float frequency = 1.0f;
-   float amplitude = 1.0f;
-   float sum = 0.0f;
-
-   for (i = 0; i < octaves; i++) {
-      float r = _stb_perlin_noise3(x*frequency,y*frequency,z*frequency,x_wrap,y_wrap,z_wrap)*amplitude;
-      r = r<0 ? -r : r; // fabs()
-      sum += r;
-      frequency *= lacunarity;
-      amplitude *= gain;
-   }
-   return sum;
-}
-// clang-format on
 
 // adapeted  stb_perlin.h
 inline float perlin_noise(const vec3f& p, const vec3i& wrap) {
   return _stb_perlin_noise3(p.x, p.y, p.z, wrap.x, wrap.y, wrap.z);
 }
 
-// adapeted  stb_perlin.h
+// ridge
 inline float perlin_ridge(const vec3f& p, float lacunarity, float gain,
     int octaves, float offset, const vec3i& wrap) {
-  return _stb_perlin_ridge_noise3(
-      p.x, p.y, p.z, lacunarity, gain, offset, octaves, wrap.x, wrap.y, wrap.z);
+  auto frequency = 1.0f;
+  auto prev      = 1.0f;
+  auto amplitude = 0.5f;
+  auto sum       = 0.0f;
+  for (auto i = 0; i < octaves; i++) {
+    auto r = perlin_noise(p * frequency, wrap);
+    r      = r < 0 ? -r : r;  // fabs()
+    r      = offset - r;
+    r      = r * r;
+    sum += r * amplitude * prev;
+    prev = r;
+    frequency *= lacunarity;
+    amplitude *= gain;
+  }
+  return sum;
 }
 
-// adapeted  stb_perlin.h
+// fmb
 inline float perlin_fbm(const vec3f& p, float lacunarity, float gain,
     int octaves, const vec3i& wrap) {
-  return _stb_perlin_fbm_noise3(
-      p.x, p.y, p.z, lacunarity, gain, octaves, wrap.x, wrap.y, wrap.z);
+  auto frequency = 1.0f;
+  auto amplitude = 1.0f;
+  auto sum       = 0.0f;
+  for (auto i = 0; i < octaves; i++) {
+    sum += perlin_noise(p * frequency, wrap) * amplitude;
+    frequency *= lacunarity;
+    amplitude *= gain;
+  }
+  return sum;
 }
 
-// adapeted  stb_perlin.h
+// turbulence
 inline float perlin_turbulence(const vec3f& p, float lacunarity, float gain,
     int octaves, const vec3i& wrap) {
-  return _stb_perlin_turbulence_noise3(
-      p.x, p.y, p.z, lacunarity, gain, octaves, wrap.x, wrap.y, wrap.z);
+  auto frequency = 1.0f;
+  auto amplitude = 1.0f;
+  auto sum       = 0.0f;
+  for (auto i = 0; i < octaves; i++) {
+    auto r = perlin_noise(p * frequency, wrap) * amplitude;
+    r      = r < 0 ? -r : r;  // fabs()
+    sum += r;
+    frequency *= lacunarity;
+    amplitude *= gain;
+  }
+  return sum;
 }
 
 }  // namespace yocto
