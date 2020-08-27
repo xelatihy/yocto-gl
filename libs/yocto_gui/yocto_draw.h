c3e204271aec768ebf3597097e3f48ec45fc4ba2
Fabio Pellacini <fabio.pellacini@gmail.com>
2020-08-27 13:19:01 +0200

    updated

--- libs/yocto_gui/yocto_draw.h
+++ libs/yocto_gui/yocto_draw.h
@@ -322,10 +322,10 @@ shade_environment* add_environment(shade_scene* scene, const frame3f& frame,
 
 // internal drawing functions
 struct shade_view {
-  frame3f      camera_frame      = {};
-  mat4f        view_matrix       = {};
-  mat4f        projection_matrix = {};
-  shade_params params            = {};
+  frame3f                     camera_frame      = {};
+  mat4f                       view_matrix       = {};
+  mat4f                       projection_matrix = {};
+  [[deprecated]] shade_params params            = {};
 };
 
 void set_scene_view_uniforms(ogl_program* program, const shade_view& view);
