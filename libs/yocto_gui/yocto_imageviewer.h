059d9a73e53f7f3fba7e8d2818af3cc5678f1020
Fabio Pellacini <fabio.pellacini@gmail.com>
2020-12-29 13:10:08 +0100

    updated

---
+++ libs/yocto_gui/yocto_imageviewer.h
@@ -0,0 +1,65 @@
+//
+// Yocto/ImageViewer: Simpler image viewer.
+//
+
+//
+// LICENSE:
+//
+// Copyright (c) 2016 -- 2020 Fabio Pellacini
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+
+#ifndef _YOCTO_IMAGEVIEWER_
+#define _YOCTO_IMAGEVIEWER_
+
+// -----------------------------------------------------------------------------
+// INCLUDES
+// -----------------------------------------------------------------------------
+
+#include <yocto_gui/yocto_opengl.h>
+#include <yocto_gui/yocto_imgui.h>
+
+#include <array>
+#include <string>
+#include <vector>
+
+// -----------------------------------------------------------------------------
+// USING DIRECTIVES
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+// using directives
+using std::array;
+using std::string;
+using std::vector;
+
+}  // namespace yocto
+
+// -----------------------------------------------------------------------------
+// IMAGE VIEWER
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+
+
+}
+
+#endif
