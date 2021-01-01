014321981a36c34ddee6347a630a3702c695aae7
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-01-01 15:54:03 +0100

    Updated copyright for new year.

--- apps/yshapeproc/yshapeproc.cpp
+++ apps/yshapeproc/yshapeproc.cpp
@@ -1,7 +1,7 @@
 //
 // LICENSE:
 //
-// Copyright (c) 2016 -- 2020 Fabio Pellacini
+// Copyright (c) 2016 -- 2021 Fabio Pellacini
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
