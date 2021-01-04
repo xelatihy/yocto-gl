a8ccb80ddd3ee041f2aa930389433a7c855b77cc
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-01-04 08:15:04 +0100

    updated

---
+++ apps/yshade/yshade_sculpt.h
@@ -0,0 +1,46 @@
+//
+// LICENSE:
+//
+// Copyright (c) 2016 -- 2021 Fabio Pellacini
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include <yocto/yocto_json.h>
+using namespace yocto;
+
+struct sculpt__params {
+  string shape   = "shape.ply"s;
+  string texture = "";
+};
+
+// Json IO
+inline void serialize_value(json_mode mode, json_value& json,
+    sculpt__params& value, const string& description) {
+  serialize_object(mode, json, value, description);
+  serialize_property(mode, json, value.shape, "shape", "Input shape.", true);
+  serialize_property(mode, json, value.texture, "texture", "Brush texturee.");
+  serialize_clipositionals(mode, json, {"shape"});
+}
+
+int run_sculpt(const sculpt__params& params);
