885b7088d5ce1fc8a0646d6870f6e64b3c48a482
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-01-17 10:55:09 +0100

    updated

--- libs/yocto/yocto_shading.h
+++ libs/yocto/yocto_shading.h
@@ -922,6 +922,7 @@ inline vec3f eval_translucent(const vec3f& color, const vec3f& normal,
 // Sample a translucency BRDF lobe.
 inline vec3f sample_translucent(const vec3f& color, const vec3f& normal,
     const vec3f& outgoing, const vec2f& rn) {
+  // TODO (fabio): fix me
   auto up_normal = dot(normal, outgoing) <= 0 ? -normal : normal;
   return sample_hemisphere_cos(-up_normal, rn);
 }
@@ -929,9 +930,36 @@ inline vec3f sample_translucent(const vec3f& color, const vec3f& normal,
 // Pdf for translucency BRDF lobe sampling.
 inline float sample_translucent_pdf(const vec3f& color, const vec3f& normal,
     const vec3f& outgoing, const vec3f& incoming) {
+  // TODO (fabio): fix me
   if (dot(normal, incoming) * dot(normal, outgoing) >= 0) return 0;
   auto up_normal = dot(normal, outgoing) <= 0 ? -normal : normal;
   return sample_hemisphere_cos_pdf(-up_normal, incoming);
+}
+
+// Evaluate a passthrough BRDF lobe.
+inline vec3f eval_passthrough(const vec3f& color, const vec3f& normal,
+    const vec3f& outgoing, const vec3f& incoming) {
+  if (dot(normal, incoming) * dot(normal, outgoing) >= 0) {
+    return vec3f{0, 0, 0};
+  } else {
+    return vec3f{1, 1, 1};
+  }
+}
+
+// Sample a passthrough BRDF lobe.
+inline vec3f sample_passthrough(
+    const vec3f& color, const vec3f& normal, const vec3f& outgoing) {
+  return -outgoing;
+}
+
+// Pdf for passthrough BRDF lobe sampling.
+inline float sample_passthrough_pdf(const vec3f& color, const vec3f& normal,
+    const vec3f& outgoing, const vec3f& incoming) {
+  if (dot(normal, incoming) * dot(normal, outgoing) >= 0) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 // Convert mean-free-path to transmission
