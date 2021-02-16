adb5c61bdbfa848a17611cbfb683b9f3856eb4dd
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-02-16 12:02:13 +0100

    updated

--- apps/yshade/yshade.cpp
+++ apps/yshade/yshade.cpp
@@ -65,7 +65,7 @@ struct shade_scene_state {
   shade_scene glscene = {};
 };
 
-static void init_glscene1(shade_scene &glscene, const sceneio_scene &ioscene,
+static void init_glscene(shade_scene &glscene, const sceneio_scene &ioscene,
     progress_callback progress_cb) {
   // handle progress
   auto progress = vec2i{
@@ -209,7 +209,7 @@ int run_shade_scene(const shade_scene_params &params) {
   // callbacks
   auto callbacks    = gui_callbacks{};
   callbacks.init_cb = [&app](gui_window *win, const gui_input &input) {
-    init_glscene1(app.glscene, app.ioscene, print_progress);
+    init_glscene(app.glscene, app.ioscene, print_progress);
   };
   callbacks.clear_cb = [&app](gui_window *win, const gui_input &input) {
     clear_scene(app.glscene);
@@ -417,118 +417,6 @@ static void convert_scene1(scene_scene &scene, const scene_shape &ioshape_,
   if (progress_cb) progress_cb("create scene", progress.x++, progress.y);
 }
 
-static void init_glscene2(shade_scene &glscene, const sceneio_scene &ioscene,
-    progress_callback progress_cb) {
-  // handle progress
-  auto progress = vec2i{
-      0, (int)ioscene.cameras.size() + (int)ioscene.materials.size() +
-             (int)ioscene.textures.size() + (int)ioscene.shapes.size() +
-             (int)ioscene.instances.size()};
-
-  // init scene
-  init_scene(glscene);
-
-  // camera
-  for (auto &iocamera : ioscene.cameras) {
-    if (progress_cb) progress_cb("convert camera", progress.x++, progress.y);
-    auto &camera = glscene.cameras.at(add_camera(glscene));
-    set_frame(camera, iocamera.frame);
-    set_lens(camera, iocamera.lens, iocamera.aspect, iocamera.film);
-    set_nearfar(camera, 0.001, 10000);
-  }
-
-  // textures
-  for (auto &iotexture : ioscene.textures) {
-    if (progress_cb) progress_cb("convert texture", progress.x++, progress.y);
-    auto  handle    = add_texture(glscene);
-    auto &gltexture = glscene.textures[handle];
-    if (!iotexture.pixelsf.empty()) {
-      set_texture(
-          gltexture, iotexture.width, iotexture.height, iotexture.pixelsf);
-    } else if (!iotexture.pixelsb.empty()) {
-      set_texture(
-          gltexture, iotexture.width, iotexture.height, iotexture.pixelsb);
-    }
-  }
-
-  // material
-  for (auto &iomaterial : ioscene.materials) {
-    if (progress_cb) progress_cb("convert material", progress.x++, progress.y);
-    auto  handle     = add_material(glscene);
-    auto &glmaterial = glscene.materials[handle];
-    set_emission(glmaterial, iomaterial.emission, iomaterial.emission_tex);
-    set_opacity(glmaterial, iomaterial.opacity, invalid_handle);
-    set_normalmap(glmaterial, iomaterial.normal_tex);
-    switch (iomaterial.type) {
-      case material_type::matte: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 0, invalid_handle);
-        set_metallic(glmaterial, 0, invalid_handle);
-        set_roughness(glmaterial, 0, invalid_handle);
-      } break;
-      case material_type::plastic: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 1, invalid_handle);
-        set_metallic(glmaterial, 0, invalid_handle);
-        set_roughness(glmaterial, iomaterial.roughness, invalid_handle);
-      } break;
-      case material_type::metal: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 0, invalid_handle);
-        set_metallic(glmaterial, 1, invalid_handle);
-        set_roughness(
-            glmaterial, iomaterial.roughness, iomaterial.roughness_tex);
-      } break;
-      case material_type::metallic: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 1, invalid_handle);
-        set_metallic(glmaterial, iomaterial.metallic, invalid_handle);
-        set_roughness(glmaterial, iomaterial.roughness, invalid_handle);
-      } break;
-      default: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 0, invalid_handle);
-        set_metallic(glmaterial, 0, invalid_handle);
-        set_roughness(
-            glmaterial, iomaterial.roughness, iomaterial.roughness_tex);
-      } break;
-    }
-  }
-
-  // shapes
-  for (auto &ioshape : ioscene.shapes) {
-    if (progress_cb) progress_cb("convert shape", progress.x++, progress.y);
-    add_shape(glscene, ioshape.points, ioshape.lines, ioshape.triangles,
-        ioshape.quads, ioshape.positions, ioshape.normals, ioshape.texcoords,
-        ioshape.colors);
-  }
-
-  // shapes
-  for (auto &ioinstance : ioscene.instances) {
-    if (progress_cb) progress_cb("convert instance", progress.x++, progress.y);
-    auto  handle     = add_instance(glscene);
-    auto &glinstance = glscene.instances[handle];
-    set_frame(glinstance, ioinstance.frame);
-    set_shape(glinstance, ioinstance.shape);
-    set_material(glinstance, ioinstance.material);
-  }
-
-  // environments
-  for (auto &ioenvironment : ioscene.environments) {
-    auto  handle        = add_environment(glscene);
-    auto &glenvironment = glscene.environments[handle];
-    set_frame(glenvironment, ioenvironment.frame);
-    set_emission(
-        glenvironment, ioenvironment.emission, ioenvironment.emission_tex);
-  }
-
-  // init environments
-  init_environments(glscene);
-
-  // done
-  if (progress_cb) progress_cb("convert scene", progress.x++, progress.y);
-}
-
 struct shade_shape_params {
   string shape = "shape.ply";
 };
@@ -563,7 +451,7 @@ int run_shade_shape(const shade_shape_params &params) {
   // callbacks
   auto callbacks    = gui_callbacks{};
   callbacks.init_cb = [&app](gui_window *win, const gui_input &input) {
-    init_glscene1(app.glscene, app.ioscene, print_progress);
+    init_glscene(app.glscene, app.ioscene, print_progress);
     app.glscene.instances[1].hidden = true;
     app.glscene.instances[2].hidden = true;
   };
@@ -1188,118 +1076,6 @@ static void convert_scene2(scene_scene &scene, const scene_shape &ioshape_,
   if (progress_cb) progress_cb("create scene", progress.x++, progress.y);
 }
 
-static void init_glscene3(shade_scene &glscene, const sceneio_scene &ioscene,
-    progress_callback progress_cb) {
-  // handle progress
-  auto progress = vec2i{
-      0, (int)ioscene.cameras.size() + (int)ioscene.materials.size() +
-             (int)ioscene.textures.size() + (int)ioscene.shapes.size() +
-             (int)ioscene.instances.size()};
-
-  // init scene
-  init_scene(glscene);
-
-  // camera
-  for (auto &iocamera : ioscene.cameras) {
-    if (progress_cb) progress_cb("convert camera", progress.x++, progress.y);
-    auto &camera = glscene.cameras.at(add_camera(glscene));
-    set_frame(camera, iocamera.frame);
-    set_lens(camera, iocamera.lens, iocamera.aspect, iocamera.film);
-    set_nearfar(camera, 0.001, 10000);
-  }
-
-  // textures
-  for (auto &iotexture : ioscene.textures) {
-    if (progress_cb) progress_cb("convert texture", progress.x++, progress.y);
-    auto  handle    = add_texture(glscene);
-    auto &gltexture = glscene.textures[handle];
-    if (!iotexture.pixelsf.empty()) {
-      set_texture(
-          gltexture, iotexture.width, iotexture.height, iotexture.pixelsf);
-    } else if (!iotexture.pixelsb.empty()) {
-      set_texture(
-          gltexture, iotexture.width, iotexture.height, iotexture.pixelsb);
-    }
-  }
-
-  // material
-  for (auto &iomaterial : ioscene.materials) {
-    if (progress_cb) progress_cb("convert material", progress.x++, progress.y);
-    auto  handle     = add_material(glscene);
-    auto &glmaterial = glscene.materials[handle];
-    set_emission(glmaterial, iomaterial.emission, iomaterial.emission_tex);
-    set_opacity(glmaterial, iomaterial.opacity, invalid_handle);
-    set_normalmap(glmaterial, iomaterial.normal_tex);
-    switch (iomaterial.type) {
-      case material_type::matte: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 0, invalid_handle);
-        set_metallic(glmaterial, 0, invalid_handle);
-        set_roughness(glmaterial, 0, invalid_handle);
-      } break;
-      case material_type::plastic: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 1, invalid_handle);
-        set_metallic(glmaterial, 0, invalid_handle);
-        set_roughness(glmaterial, iomaterial.roughness, invalid_handle);
-      } break;
-      case material_type::metal: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 0, invalid_handle);
-        set_metallic(glmaterial, 1, invalid_handle);
-        set_roughness(
-            glmaterial, iomaterial.roughness, iomaterial.roughness_tex);
-      } break;
-      case material_type::metallic: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 1, invalid_handle);
-        set_metallic(glmaterial, iomaterial.metallic, invalid_handle);
-        set_roughness(glmaterial, iomaterial.roughness, invalid_handle);
-      } break;
-      default: {
-        set_color(glmaterial, iomaterial.color, iomaterial.color_tex);
-        set_specular(glmaterial, 0, invalid_handle);
-        set_metallic(glmaterial, 0, invalid_handle);
-        set_roughness(
-            glmaterial, iomaterial.roughness, iomaterial.roughness_tex);
-      } break;
-    }
-  }
-
-  // shapes
-  for (auto &ioshape : ioscene.shapes) {
-    if (progress_cb) progress_cb("convert shape", progress.x++, progress.y);
-    add_shape(glscene, ioshape.points, ioshape.lines, ioshape.triangles,
-        ioshape.quads, ioshape.positions, ioshape.normals, ioshape.texcoords,
-        ioshape.colors);
-  }
-
-  // shapes
-  for (auto &ioinstance : ioscene.instances) {
-    if (progress_cb) progress_cb("convert instance", progress.x++, progress.y);
-    auto  handle     = add_instance(glscene);
-    auto &glinstance = glscene.instances[handle];
-    set_frame(glinstance, ioinstance.frame);
-    set_shape(glinstance, ioinstance.shape);
-    set_material(glinstance, ioinstance.material);
-  }
-
-  // environments
-  for (auto &ioenvironment : ioscene.environments) {
-    auto  handle        = add_environment(glscene);
-    auto &glenvironment = glscene.environments[handle];
-    set_frame(glenvironment, ioenvironment.frame);
-    set_emission(
-        glenvironment, ioenvironment.emission, ioenvironment.emission_tex);
-  }
-
-  // init environments
-  init_environments(glscene);
-
-  // done
-  if (progress_cb) progress_cb("convert scene", progress.x++, progress.y);
-}
-
 // To make the stroke sampling (position, normal) following the mouse
 pair<vector<shape_point>, vec2f> sample_stroke(const shape_bvh &bvh,
     const scene_shape &shape, const vec2f &last_uv, const vec2f &mouse_uv,
@@ -1455,7 +1231,7 @@ int run_shade_sculpt(const shade_sculpt_params &params_) {
   // callbacks
   auto callbacks    = gui_callbacks{};
   callbacks.init_cb = [&app](gui_window *win, const gui_input &input) {
-    init_glscene1(app.glscene, app.ioscene, print_progress);
+    init_glscene(app.glscene, app.ioscene, print_progress);
   };
   callbacks.clear_cb = [&app](gui_window *win, const gui_input &input) {
     clear_scene(app.glscene);
