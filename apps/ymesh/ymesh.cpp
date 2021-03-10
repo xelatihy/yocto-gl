0212e088de290f93e690debb9952a02bec98359f
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-03-10 17:42:52 +0100

    updated

--- apps/ymesh/ymesh.cpp
+++ apps/ymesh/ymesh.cpp
@@ -187,7 +187,7 @@ void add_command(cli_command& cli, const string& name, glpath_params& value,
 #ifndef YOCTO_OPENGL
 
 // view shapes
-int run_glview(const glview_params& params) {
+int run_glpath(const glpath_params& params) {
   return print_fatal("Opengl not compiled");
 }
 
@@ -268,6 +268,193 @@ int run_glpath(const glpath_params &params) {
 
   // create scene
   auto scene = make_pathscene(ioshape);
+
+  // bvh
+  auto &shape = scene.shapes.at(0);
+  auto  bvh   = make_triangles_bvh(shape.triangles, shape.positions, {});
+
+  // stroke
+  auto stroke = vector<shape_point>{};
+
+  // geodesic solver
+  auto adjacencies = face_adjacencies(shape.triangles);
+  auto solver      = make_dual_geodesic_solver(
+      shape.triangles, shape.positions, adjacencies);
+  auto bezier = true;
+
+  // bezier algos
+  auto params1      = spline_params{};
+  params1.algorithm = spline_algorithm::de_casteljau_uniform;
+  auto params2      = spline_params{};
+  params2.algorithm = spline_algorithm::de_casteljau_adaptive;
+  auto params3      = spline_params{};
+  params3.algorithm = spline_algorithm::lane_riesenfeld_uniform;
+  auto params4      = spline_params{};
+  params4.algorithm = spline_algorithm::lane_riesenfeld_adaptive;
+
+  // run viewer
+  glview_scene(
+      scene, params.shape, "",
+      [&](gui_window *win, const gui_input &input, scene_model &scene,
+          shade_scene &glscene) {},
+      [&](gui_window *win, const gui_input &input, scene_model &scene,
+          shade_scene &glscene) {
+        auto &shape   = scene.shapes.at(0);
+        auto &camera  = scene.cameras.at(0);
+        auto  updated = false;
+        if (input.mouse_left && input.modifier_ctrl) {
+          if (input.modifier_shift) {
+            stroke.clear();
+            updated = true;
+          } else {
+            auto mouse_uv = vec2f{
+                input.mouse_pos.x / float(input.window_size.x),
+                input.mouse_pos.y / float(input.window_size.y)};
+            auto ray  = camera_ray(camera.frame, camera.lens, camera.aspect,
+                camera.film, mouse_uv);
+            auto isec = intersect_triangles_bvh(
+                bvh, shape.triangles, shape.positions, ray, false);
+            if (isec.hit) {
+              if (stroke.empty() || stroke.back().element != isec.element ||
+                  stroke.back().uv != isec.uv) {
+                stroke.push_back({isec.element, isec.uv});
+                updated = true;
+              }
+            }
+          }
+        }
+        if (updated) {
+          auto positions = vector<vec3f>{};
+          for (auto [element, uv] : stroke) {
+            positions.push_back(eval_position(shape, element, uv));
+          }
+          auto &points = scene.shapes.at(1);
+          points       = points_to_spheres(positions);
+          set_positions(glscene.shapes.at(1), points.positions);
+          set_normals(glscene.shapes.at(1), points.normals);
+          set_texcoords(glscene.shapes.at(1), points.texcoords);
+          set_quads(glscene.shapes.at(1), points.quads);
+          glscene.shapes.at(1).point_size = 10;
+          auto path = bezier ? compute_bezier_path(solver, shape.triangles,
+                                   shape.positions, adjacencies,
+                                   (vector<mesh_point> &)stroke, params1)
+                             : compute_shortest_path(solver, shape.triangles,
+                                   shape.positions, adjacencies,
+                                   (vector<mesh_point> &)stroke);
+          auto ppositions = vector<vec3f>{};
+          for (auto [element, uv] : path) {
+            ppositions.push_back(eval_position(shape, element, uv));
+          }
+          auto &lines = scene.shapes.at(2);
+          lines       = lines_to_cylinders(ppositions);
+          set_positions(glscene.shapes.at(2), lines.positions);
+          set_normals(glscene.shapes.at(2), lines.normals);
+          set_texcoords(glscene.shapes.at(2), lines.texcoords);
+          set_quads(glscene.shapes.at(2), lines.quads);
+        }
+      });
+
+  // done
+  return 0;
+}
+
+#endif
+
+struct glpathd_params {
+  string shape = "shape.ply";
+};
+
+// Cli
+void add_command(cli_command& cli, const string& name, glpathd_params& value,
+    const string& usage) {
+  auto& cmd = add_command(cli, name, usage);
+  add_argument(cmd, "shape", value.shape, "Input shape.");
+}
+
+#ifndef YOCTO_OPENGL
+
+// view shapes
+int run_glpathd(const glpathd_params& params) {
+  return print_fatal("Opengl not compiled");
+}
+
+#else
+
+static scene_model make_pathdscene(const scene_shape &ioshape_) {
+  // Frame camera
+  auto camera_frame = [](float lens, float aspect,
+                          float film = 0.036) -> frame3f {
+    auto camera_dir  = normalize(vec3f{0, 0.5, 1});
+    auto bbox_radius = 2.0f;
+    auto camera_dist = bbox_radius * lens / (film / aspect);
+    return lookat_frame(camera_dir * camera_dist, {0, 0, 0}, {0, 1, 0});
+  };
+
+  // init scene
+  auto scene = scene_model{};
+
+  // rescale shape to unit
+  auto ioshape = ioshape_;
+  auto bbox    = invalidb3f;
+  for (auto &pos : ioshape.positions) bbox = merge(bbox, pos);
+  for (auto &pos : ioshape.positions) pos -= center(bbox);
+  for (auto &pos : ioshape.positions) pos /= max(size(bbox));
+  // TODO(fabio): this should be a math function
+
+  // camera
+  auto &camera  = scene.cameras.emplace_back();
+  camera.frame  = camera_frame(0.050, 16.0f / 9.0f, 0.036);
+  camera.lens   = 0.050;
+  camera.aspect = 16.0f / 9.0f;
+  camera.film   = 0.036;
+  camera.focus  = length(camera.frame.o - center(bbox));
+
+  // material
+  auto &shape_material      = scene.materials.emplace_back();
+  shape_material.type       = scene_material_type::glossy;
+  shape_material.color      = {0.5, 1, 0.5};
+  shape_material.roughness  = 0.2;
+  auto &points_material     = scene.materials.emplace_back();
+  points_material.type      = scene_material_type::glossy;
+  points_material.color     = {1, 0.5, 0.5};
+  points_material.roughness = 0.2;
+  auto &lines_material      = scene.materials.emplace_back();
+  lines_material.type       = scene_material_type::glossy;
+  lines_material.color      = {0.5, 0.5, 1};
+  lines_material.roughness  = 0.2;
+
+  // shapes
+  scene.shapes.emplace_back(ioshape);
+  scene.shapes.emplace_back(points_to_spheres({{0, 0, 0}}));
+  scene.shapes.emplace_back(lines_to_cylinders({{0, 0, 0}, {0, 0, 0}}));
+
+  // instances
+  auto &shape_instance     = scene.instances.emplace_back();
+  shape_instance.shape     = 0;
+  shape_instance.material  = 0;
+  auto &points_instance    = scene.instances.emplace_back();
+  points_instance.shape    = 1;
+  points_instance.material = 1;
+  auto &lines_instance     = scene.instances.emplace_back();
+  lines_instance.shape     = 2;
+  lines_instance.material  = 2;
+
+  // done
+  return scene;
+}
+
+int run_glpathd(const glpathd_params &params) {
+  // loading shape
+  auto ioerror = ""s;
+  auto ioshape = scene_shape{};
+  if (!load_shape(params.shape, ioshape, ioerror, true)) print_fatal(ioerror);
+  if (!ioshape.quads.empty()) {
+    ioshape.triangles = quads_to_triangles(ioshape.quads);
+    ioshape.quads     = {};
+  }
+
+  // create scene
+  auto scene = make_pathdscene(ioshape);
 
   // bvh
   auto &shape = scene.shapes.at(0);
@@ -1092,6 +1279,7 @@ struct app_params {
   view_params     view     = {};
   glview_params   glview   = {};
   glpath_params   glpath   = {};
+  glpathd_params  glpathd  = {};
   glsculpt_params glsculpt = {};
 };
 
@@ -1103,6 +1291,7 @@ void add_commands(cli_command& cli, const string& name, app_params& value,
   add_command(cli, "view", value.view, "View shapes.");
   add_command(cli, "glview", value.glview, "View shapes with OpenGL.");
   add_command(cli, "glpath", value.glpath, "Trace paths with OpenGL.");
+  add_command(cli, "glpathd", value.glpath, "Trace debug paths with OpenGL.");
   add_command(cli, "glsculpt", value.glsculpt, "Sculpt meshes with OpenGL.");
 }
 
@@ -1125,6 +1314,8 @@ int main(int argc, const char* argv[]) {
     return run_glview(params.glview);
   } else if (params.command == "glpath") {
     return run_glpath(params.glpath);
+  } else if (params.command == "glpathd") {
+    return run_glpathd(params.glpathd);
   } else {
     return print_fatal("unknown command " + params.command);
   }
