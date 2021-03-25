c9c15caec9566d723df1da86225352cc5e444960
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-03-25 13:57:03 +0100

    updated

--- libs/yocto_gui/yocto_glview.cpp
+++ libs/yocto_gui/yocto_glview.cpp
@@ -2409,8 +2409,228 @@ struct ImGuiAppLog {
   }
 };
 
+}  // namespace yocto
+
+// -----------------------------------------------------------------------------
+// OPENGL WIDGETS
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+enum struct gui_param_type {
+  // clang-format off
+  value1f, value2f, value3f, value4f, 
+  value1i, value2i, value3i, value4i, 
+  value1s, value1b
+  // clang-format on
+};
+
+struct glwidgets_param {
+  // constructors
+  glwidgets_param()
+      : type{gui_param_type::value1f}
+      , valuef{0, 0, 0, 0}
+      , minmaxf{0, 0}
+      , readonly{true} {}
+  glwidgets_param(
+      float value, const vec2f& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value1f}
+      , valuef{value, 0, 0, 0}
+      , minmaxf{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(
+      vec2f value, const vec2f& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value2f}
+      , valuef{value.x, value.y, 0, 0}
+      , minmaxf{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(
+      vec3f value, const vec2f& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value3f}
+      , valuef{value.x, value.y, value.z}
+      , minmaxf{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(
+      vec4f value, const vec2f& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value4f}
+      , valuef{value.x, value.y, value.z, value.w}
+      , minmaxf{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(vec3f value, bool color, bool readonly = false)
+      : type{gui_param_type::value3f}
+      , valuef{value.x, value.y, value.z, 1}
+      , color{color}
+      , readonly{readonly} {}
+  glwidgets_param(vec4f value, bool color, bool readonly = false)
+      : type{gui_param_type::value4f}
+      , valuef{value.x, value.y, value.z, value.w}
+      , color{color}
+      , readonly{readonly} {}
+  glwidgets_param(
+      int value, const vec2i& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value1i}
+      , valuei{value, 0, 0, 0}
+      , minmaxi{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(
+      vec2i value, const vec2i& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value2i}
+      , valuei{value.x, value.y, 0, 0}
+      , minmaxi{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(
+      vec3i value, const vec2i& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value3i}
+      , valuei{value.x, value.y, value.z, 0}
+      , minmaxi{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(
+      vec4i value, const vec2i& minmax = {0, 0}, bool readonly = false)
+      : type{gui_param_type::value4i}
+      , valuei{value.x, value.y, value.z, value.w}
+      , minmaxi{minmax}
+      , readonly{readonly} {}
+  glwidgets_param(bool value, bool readonly = false)
+      : type{gui_param_type::value1b}, valueb{value}, readonly{readonly} {}
+  glwidgets_param(const string& value, bool readonly = false)
+      : type{gui_param_type::value1s}, values{value}, readonly{readonly} {}
+  glwidgets_param(
+      const string& value, const vector<string>& labels, bool readonly = false)
+      : type{gui_param_type::value1s}
+      , values{value}
+      , labels{labels}
+      , readonly{readonly} {}
+  glwidgets_param(
+      int value, const vector<string>& labels, bool readonly = false)
+      : type{gui_param_type::value1i}
+      , valuei{value, 0, 0, 0}
+      , labels{labels}
+      , readonly{readonly} {}
+  template <typename T, typename = std::enable_if_t<std::is_enum_v<T>>>
+  glwidgets_param(T value, const vector<string>& labels, bool readonly = false)
+      : type{gui_param_type::value1i}
+      , valuei{(int)value, 0, 0, 0}
+      , labels{labels}
+      , readonly{readonly} {}
+
+  // conversions
+  operator float() const {
+    check_type(gui_param_type::value1f);
+    return valuef.x;
+  }
+  operator vec2f() const {
+    check_type(gui_param_type::value2f);
+    return {valuef.x, valuef.y};
+  }
+  operator vec3f() const {
+    check_type(gui_param_type::value3f);
+    return {valuef.x, valuef.y, valuef.z};
+  }
+  operator vec4f() const {
+    check_type(gui_param_type::value4f);
+    return {valuef.x, valuef.y, valuef.z, valuef.w};
+  }
+  operator int() const {
+    check_type(gui_param_type::value1i);
+    return valuei.x;
+  }
+  operator vec2i() const {
+    check_type(gui_param_type::value2i);
+    return {valuei.x, valuei.y};
+  }
+  operator vec3i() const {
+    check_type(gui_param_type::value3i);
+    return {valuei.x, valuei.y, valuei.z};
+  }
+  operator vec4i() const {
+    check_type(gui_param_type::value4i);
+    return {valuei.x, valuei.y, valuei.z, valuei.w};
+  }
+  operator bool() const {
+    check_type(gui_param_type::value1b);
+    return valueb;
+  }
+  operator string() const {
+    check_type(gui_param_type::value1s);
+    return values;
+  }
+  template <typename T, typename = std::enable_if_t<std::is_enum_v<T>>>
+  operator T() const {
+    check_type(gui_param_type::value1i);
+    return (T)valuei.x;
+  }
+
+  // type checking
+  void check_type(gui_param_type type) const {
+    if (type != this->type) throw std::invalid_argument{"bad gui type"};
+  }
+
+  // value
+  gui_param_type type   = gui_param_type::value1f;
+  vec4f          valuef = {0, 0, 0, 0};
+  vec4i          valuei = {0, 0, 0, 0};
+  bool           valueb = false;
+  string         values = "";
+
+  // display properties
+  vec2f          minmaxf  = {0, 0};
+  vec2i          minmaxi  = {0, 0};
+  bool           color    = false;
+  vector<string> labels   = {};
+  bool           readonly = false;
+};
+
+struct glwidgets_params {
+  using container      = vector<pair<string, glwidgets_param>>;
+  using iterator       = container::iterator;
+  using const_iterator = container::const_iterator;
+
+  glwidgets_params() {}
+
+  bool   empty() const { return items.empty(); }
+  size_t size() const { return items.size(); }
+
+  glwidgets_param& operator[](const string& key) {
+    auto item = find(key);
+    if (item == end()) return items.emplace_back(key, glwidgets_param{}).second;
+    return item->second;
+  }
+  const glwidgets_param& operator[](const string& key) const { return at(key); }
+
+  glwidgets_param& at(const string& key) {
+    auto item = find(key);
+    if (item == end()) throw std::out_of_range{"key not found " + key};
+    return item->second;
+  }
+  const glwidgets_param& at(const string& key) const {
+    auto item = find(key);
+    if (item == end()) throw std::out_of_range{"key not found " + key};
+    return item->second;
+  }
+
+  iterator find(const string& key) {
+    for (auto iterator = items.begin(); iterator != items.end(); ++iterator) {
+      if (iterator->first == key) return iterator;
+    }
+    return items.end();
+  }
+  const_iterator find(const string& key) const {
+    for (auto iterator = items.begin(); iterator != items.end(); ++iterator) {
+      if (iterator->first == key) return iterator;
+    }
+    return items.end();
+  }
+
+  iterator       begin() { return items.begin(); }
+  iterator       end() { return items.end(); }
+  const_iterator begin() const { return items.begin(); }
+  const_iterator end() const { return items.end(); }
+
+ private:
+  vector<pair<string, glwidgets_param>> items;
+};
+
 // draw param
-bool draw_glparam(const string& name, gui_param& param) {
+bool draw_glparam(const string& name, glwidgets_param& param) {
   auto copy = param;
   switch (param.type) {
     case gui_param_type::value1f:
@@ -2559,7 +2779,7 @@ bool draw_glparam(const string& name, gui_param& param) {
 }
 
 // draw params
-bool draw_glparams(const string& name, gui_params& params) {
+bool draw_glparams(const string& name, glwidgets_params& params) {
   auto edited = false;
   if (begin_glheader(name.c_str())) {
     for (auto& [name, param] : params) {
