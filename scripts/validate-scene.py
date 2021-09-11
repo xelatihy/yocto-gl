cee3da1ecb8302aa43e516daf4bb3c85d42d4bba
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-09-11 18:03:02 +0200

    updated

--- scripts/validate-scene.py
+++ scripts/validate-scene.py
@@ -1,15 +1,18 @@
 #! /usr/bin/env python3 -B
 
 import jsonschema, json, typer
+from typing import List
 
-def main(input: str, schema: str):
-  with open(input) as f:
-    json_input = json.load(f)
+def main(schema: str, inputs: List[str]):
   with open(schema) as f:
     json_schema = json.load(f)
-  validator = jsonschema.Draft7Validator(json_schema)
-  for error in validator.iter_errors(json_input):
-     print(error.message)
+  for input in inputs:
+    print(f'--- {input}')
+    with open(input) as f:
+      json_input = json.load(f)
+    validator = jsonschema.Draft7Validator(json_schema)
+    for error in validator.iter_errors(json_input):
+      print(error.message)
 
 if __name__ == '__main__':
   typer.run(main)
