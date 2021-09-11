a85e139e39879ca02a97887e2ab4d4307ad5f2e2
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-09-11 17:46:49 +0200

    updated

---
+++ scripts/validate-scene.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3 -B
+
+import jsonschema, json, typer
+
+def main(input: str, schema: str):
+  with open(input) as f:
+    json_input = json.load(f)
+  with open(schema) as f:
+    json_schema = json.load(f)
+  validator = jsonschema.Draft7Validator(json_schema)
+  for error in validator.iter_errors(json_input):
+     print(error.path)
+
+if __name__ == '__main__':
+  typer.run(main)
