4500538a5e21ddd89ae53397eec2fe3cdeaea2e8
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-09-13 20:41:31 +0200

    updatd

--- libs/yocto/yocto_modelio.cpp
+++ libs/yocto/yocto_modelio.cpp
@@ -41,6 +41,10 @@
 #include <unordered_set>
 #include <utility>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include "ext/fast_float.h"
 
 // -----------------------------------------------------------------------------
