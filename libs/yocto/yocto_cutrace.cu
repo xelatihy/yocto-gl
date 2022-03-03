c72e1761c6573b45290e6993488f2cd43e365197
Fabio Pellacini <fabio.pellacini@gmail.com>
2022-03-03 04:57:15 +0100

    updated

--- libs/yocto/yocto_cutrace.cu
+++ libs/yocto/yocto_cutrace.cu
@@ -2879,6 +2879,87 @@ struct trace_result {
   vec3f normal   = {0, 0, 0};
 };
 
+// Recursive path tracing.
+static trace_result trace_naive(const scene_data& scene, const trace_bvh& bvh,
+    const trace_lights& lights, const ray3f& ray_, rng_state& rng,
+    const trace_params& params) {
+  // initialize
+  auto radiance   = vec3f{0, 0, 0};
+  auto weight     = vec3f{1, 1, 1};
+  auto ray        = ray_;
+  auto hit        = false;
+  auto hit_albedo = vec3f{0, 0, 0};
+  auto hit_normal = vec3f{0, 0, 0};
+  auto opbounce   = 0;
+
+  // trace  path
+  for (auto bounce = 0; bounce < params.bounces; bounce++) {
+    // intersect next point
+    auto intersection = intersect_scene(bvh, scene, ray);
+    if (!intersection.hit) {
+      if (bounce > 0 || !params.envhidden)
+        radiance += weight * eval_environment(scene, ray.d);
+      break;
+    }
+
+    // prepare shading point
+    auto outgoing = -ray.d;
+    auto position = eval_shading_position(scene, intersection, outgoing);
+    auto normal   = eval_shading_normal(scene, intersection, outgoing);
+    auto material = eval_material(scene, intersection);
+
+    // handle opacity
+    if (material.opacity < 1 && rand1f(rng) >= material.opacity) {
+      if (opbounce++ > 128) break;
+      ray = {position + ray.d * 1e-2f, ray.d};
+      bounce -= 1;
+      continue;
+    }
+
+    // set hit variables
+    if (bounce == 0) {
+      hit        = true;
+      hit_albedo = material.color;
+      hit_normal = normal;
+    }
+
+    // accumulate emission
+    radiance += weight * eval_emission(material, normal, outgoing);
+
+    // next direction
+    auto incoming = vec3f{0, 0, 0};
+    if (material.roughness != 0) {
+      incoming = sample_bsdfcos(
+          material, normal, outgoing, rand1f(rng), rand2f(rng));
+      if (incoming == vec3f{0, 0, 0}) break;
+      auto pdf = sample_bsdfcos_pdf(material, normal, outgoing, incoming);
+      if (pdf == 0) break;
+      weight *= eval_bsdfcos(material, normal, outgoing, incoming) / pdf;
+    } else {
+      incoming = sample_delta(material, normal, outgoing, rand1f(rng));
+      if (incoming == vec3f{0, 0, 0}) break;
+      auto pdf = sample_delta_pdf(material, normal, outgoing, incoming);
+      if (pdf == 0) break;
+      weight *= eval_delta(material, normal, outgoing, incoming) / pdf;
+    }
+
+    // check weight
+    if (weight == vec3f{0, 0, 0}) break;
+
+    // russian roulette
+    if (bounce > 3) {
+      auto rr_prob = min((float)0.99, max(weight));
+      if (rand1f(rng) >= rr_prob) break;
+      weight *= 1 / rr_prob;
+    }
+
+    // setup next iteration
+    ray = {position, incoming};
+  }
+
+  return {radiance, hit, hit_albedo, hit_normal};
+}
+
 // Eyelight for quick previewing.
 static trace_result trace_eyelight(const scene_data& scene,
     const trace_bvh& bvh, const trace_lights& lights, const ray3f& ray_,
@@ -3039,7 +3120,7 @@ static void trace_pixel(cutrace_state& state, const cutrace_scene& scene,
   auto ray = eval_camera(camera, uv, {0, 0});
 
   // shade
-  auto result = trace_eyelight(scene, bvh, lights, ray, rng, params);
+  auto result = trace_naive(scene, bvh, lights, ray, rng, params);
   auto color  = result.radiance;
   state.image[i + j * state.width] += {color.x, color.y, color.z, 1};
 }
