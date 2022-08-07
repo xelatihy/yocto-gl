9645a283f443c50aecac0b4c6f03bf1d8344d457
Fabio Pellacini <fabio.pellacini@gmail.com>
2022-08-07 09:44:52 +0200

    updated

--- libs/yocto/yocto_bvh.h
+++ libs/yocto/yocto_bvh.h
@@ -66,18 +66,6 @@ using std::vector;
 // -----------------------------------------------------------------------------
 namespace yocto {
 
-// BVH tree node containing its bounds, indices to the BVH arrays of either
-// primitives or internal nodes, the node element type,
-// and the split axis. Leaf and internal nodes are identical, except that
-// indices refer to primitives for leaf nodes or other nodes for internal nodes.
-struct bvh_node {
-  bbox3f  bbox     = invalidb3f;
-  int32_t start    = 0;
-  int16_t num      = 0;
-  int8_t  axis     = 0;
-  bool    internal = false;
-};
-
 // BVH tree stored as a node array with the tree structure is encoded using
 // array indices. BVH nodes indices refer to either the node array,
 // for internal nodes, or the primitive arrays, for leaf nodes.
